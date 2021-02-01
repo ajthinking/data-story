a7f1c49f0c994877ec9aacbbcf32aec61bacc094
Anders Jurisoo <jurisoo@hotmail.com>
2021-02-01 09:18:48 +0100

    Immensely helpful render & boot tests

--- tests/Feature/WorkbenchTest.php
+++ tests/Feature/WorkbenchTest.php
@@ -4,10 +4,15 @@ namespace DataStory\Tests\Feature;
 
 use DataStory\Tests\TestCase;
 
-class RenderTest extends TestCase
+class WorkbenchTest extends TestCase
 {
     public function test_that_it_can_render_the_workbench()
     {
         $this->get('/datastory')->assertStatus(200);
     }
+
+    public function test_that_the_laravel_server_can_boot()
+    {
+        $this->post('/datastory/api/boot')->assertStatus(200);
+    }    
 }
