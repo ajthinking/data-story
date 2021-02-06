4c8158a353ec0df78a2c40bea11de87c7df0b80b
Anders Jurisoo <jurisoo@hotmail.com>
2021-02-06 19:31:27 +0100

    No test found use annotation

--- tests/Unit/Nodes/RayTest.php
+++ tests/Unit/Nodes/RayTest.php
@@ -6,6 +6,7 @@ use DataStory\Tests\TestCase;
 
 class RayTest extends TestCase
 {
+    /** @test */
     public function the_ray_node_requires_spaties_tool_but_wont_crash_if_its_missing()
     {
         Diagram::test()
