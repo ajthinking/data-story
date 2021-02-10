26903f025029014a5bd061e20ad25f3c7d338f6c
Anders Jurisoo <jurisoo@hotmail.com>
2021-02-10 17:00:10 +0100

    Orchestra dusk can run alongside unit/feature tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -34,5 +34,20 @@ class TestCase extends \Orchestra\Testbench\TestCase
             'database' => ':memory:',
             'prefix'   => '',
         ]);
+    }
+    
+    /**
+    * Make sure all integration tests use the same Laravel "skeleton" files.
+    * This avoids duplicate classes during migrations.
+    *
+    * Overrides \Orchestra\Testbench\Dusk\TestCase::getBasePath
+    *       and \Orchestra\Testbench\Concerns\CreatesApplication::getBasePath
+    *
+    * @return string
+    */
+    protected function getBasePath()
+    {
+        // Adjust this path depending on where your override is located.
+        return __DIR__.'/../vendor/orchestra/testbench-dusk/laravel'; 
     }    
 }
