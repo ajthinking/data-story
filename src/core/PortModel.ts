66c677d7aef8e82266fbec9b8f540dd3b755adc1
Anders Jurisoo <jurisoo@hotmail.com>
2021-04-17 06:48:30 +0200

    TSify PortModel

--- src/core/PortModel.ts
+++ src/core/PortModel.ts
@@ -1,11 +1,11 @@
-import { DefaultPortModel} from '@projectstorm/react-diagrams';
+import { DefaultPortModel, PortModelOptions} from '@projectstorm/react-diagrams';
 
 export default class PortModel extends DefaultPortModel {
-	constructor(options = {}) {
+	constructor(options : any = {}) {
 		super({
 			...options,
             // Make id easier on humans
             id: `Port_${options.name}_on_${options.parent.options.id}}`
-        });
+        } as PortModelOptions);
     }
 }
