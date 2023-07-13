76c85fc5dc2a46f278499224d1f04d2b918b3b30
ld-codes <123950213+ld-codes@users.noreply.github.com>
2023-07-13 21:13:08 +0800

    add ui path to tailwind config

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,14 +1,7 @@
-"use client"
+"use client";
 
-import { Hey, DataStory } from "@data-story/ui";
+import { DataStory } from "@data-story/ui";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen">
-      <Hey />
-      <div className="w-full h-screen bg-vsCodeWarmGray-900">
-        <div className="w-full h-5/6">{<DataStory />}</div>
-      </div>
-    </main>
-  );
+  return <main className="flex h-screen">{<DataStory />}</main>;
 }
--- packages/ui/src/components/Hey.tsx
+++
@@ -1,6 +0,0 @@
-import { useState } from "react";
-export function Hey() {
-  const [message, setMessage] = useState("");
-
-  return <h1>Hey {message}!</h1>;
-}
