dfc8d1e8b4a763a974c34d66a4943dece1b0ce36
Anders Jurisoo <jurisoo@hotmail.com>
2023-07-15 09:42:22 +0200

    Temporary style fix

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import { DataStory } from "@data-story/ui";
+import '@data-story/ui/dist/data-story.css';
 
 export default function Home() {
-  return <main className="flex h-screen">{<DataStory />}</main>;
+  return <main className="flex h-screen">
+    <DataStory />
+  </main>;
 }
