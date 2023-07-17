81b7f361fbb376de622e2e91dcb424a14daae19e
Anders Jürisoo <jurisoo@hotmail.com>
2023-07-17 21:52:23 +0200

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>DataStory</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/ajthinking/data-story-docs',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/ajthinking/data-story-docs',
   footer: {
     text: 'Nextra Docs Template',
   },
