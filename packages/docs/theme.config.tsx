2883467246dd0efec66acfb0f8ff73b02ba1c14f
Anders Jurisoo <jurisoo@hotmail.com>
2023-08-19 00:21:00 +0200

    Fix links

--- packages/docs/theme.config.tsx
+++ packages/docs/theme.config.tsx
@@ -4,15 +4,15 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>DataStory</span>,
   project: {
-    link: 'https://github.com/ajthinking/data-story-docs',
+    link: 'https://github.com/ajthinking/data-story',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/ajthinking/data-story-docs',
-  footer: {
-    text: 'Built with Nextra',
-  },
+  docsRepositoryBase: 'https://github.com/ajthinking/data-story/packages/docs',
+  // footer: {
+  //   text: 'Built with Nextra',
+  // },
 }
 
 export default config
