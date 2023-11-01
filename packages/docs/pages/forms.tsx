5f153972e5e720295083fe3f89fd9148e45c3fba
Anders Jurisoo <jurisoo@hotmail.com>
2023-11-01 08:50:44 +0100

    Remove redundant code

--- packages/docs/pages/forms.tsx
+++ packages/docs/pages/forms.tsx
@@ -1,8 +1,5 @@
-"use client";
-
-import { Application, coreNodeProvider } from "@data-story/core";
-import { DataStory, DynamicField, ExampleForm } from "@data-story/ui";
-import '@data-story/ui/dist/data-story.css';
+import { ExampleForm } from "@data-story/ui";
+import "@data-story/ui/dist/data-story.css";
 
 export default function Home() {
   return <div className="flex w-full bg-blue-500 flex-col items-center">
