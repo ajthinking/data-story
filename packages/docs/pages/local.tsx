724fbc6e8fab12dff5d2073a7e18f90f1c7137a0
Anders Jurisoo <jurisoo@hotmail.com>
2023-11-04 23:29:48 +0100

    Change height

--- packages/docs/pages/local.tsx
+++ packages/docs/pages/local.tsx
@@ -2,7 +2,7 @@ import { DataStory } from '@data-story/ui';
 import '@data-story/ui/dist/data-story.css';
 
 export default function Home() {
-  return <div className="w-full" style={{ height: '100vh' }}>
+  return <div className="w-full" style={{ height: '50vh' }}>
     <DataStory server={{type: 'SOCKET', url: 'http://localhost:3000'}} />
   </div>
 }
