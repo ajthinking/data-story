c41c7014481b13b5a3b8ceb98ff74b8fd03b68a4
Anders Jürisoo <jurisoo@hotmail.com>
2023-12-01 15:46:12 +0100

    uniform tab style

--- packages/ui/src/components/DataStory/modals/nodeSettingsModal/tabs/Code.tsx
+++ packages/ui/src/components/DataStory/modals/nodeSettingsModal/tabs/Code.tsx
@@ -2,22 +2,23 @@ import React from 'react';
 import { DataStoryNode } from '../../../../Node/DataStoryNode';
 
 export function Code({
-  node
+  node,
+  register
 }: {
   node: DataStoryNode,
   register: any
 }) {
 
-  return <div className="max-h-128 overflow-y-scroll relative pb-6 pt-4 px-6 flex-auto space-y-1 text-sm font-mono text-gray-800">    
+  return <div className="max-h-128 overflow-y-scroll relative pb-6 pt-4 px-6 flex-auto space-y-1 text-sm font-mono text-gray-800">
     <div
-    className="flex flex-col"
+      className="flex flex-col"
     >
-      <label className="mt-2 mb-1 text-xs text-gray-400">Node as JSON</label>
+      <label className="mt-2 mb-1 text-xs text-gray-400">Output Schema</label>
       <textarea
-        readOnly
-        placeholder={`{ "someProperty": "string"}`}
-        className="w-full h-96 text-xs text-gray-400 font-mono px-2 py-1 border rounded border-blue-200"
-        value={JSON.stringify(node, null, 2)}
+        {...register('code')}
+        placeholder={'[]'}
+        className="w-full bg-white h-48 text-xs text-gray-400 px-2 py-1 border rounded border-blue-200"
+        defaultValue={JSON.stringify(node, null, 2)}
       ></textarea>
     </div>
   </div>;
