15d82f1b896a7d5fd5a8bfe3fbd6e394a691b819
stone-lyl <stone-lyl@outlook.com>
2023-12-07 17:38:46 +0800

    test: add runModal component test

--- packages/ui/src/components/DataStory/DataStory.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { DataStory } from './DataStory'
-
-describe('<DataStory />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<DataStory />)
-  })
-})
---
+++ packages/ui/src/components/DataStory/modals/RunModal.cy.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { RunModal } from './runModal'
+import { DataStoryProvider } from '../store/store';
+import * as store from '../store/store';
+
+function mountRunModal(): void {
+  cy.mount(
+    <DataStoryProvider>
+      <RunModal setShowModal={(show) => show}/>
+    </DataStoryProvider>
+  )
+}
+
+function getEl(name: 'run-modal' | 'run-modal-server' | 'run-modal-button') {
+  return cy.dataCy(name);
+}
+
+describe('<RunModal />', () => {
+
+  it('renders', () => {
+
+    mountRunModal();
+
+    getEl('run-modal').should('exist');
+    getEl('run-modal-server').should('contain', 'localhost');
+  });
+
+  it('render type is JS', () => {
+
+    cy.stub(store, 'createStore').returns(() => {
+      return {
+        serverConfig: { type: 'JS' },
+      };
+    });
+
+    mountRunModal();
+
+    getEl('run-modal-server').should('have.text', 'JS');
+  });
+
+  it('click run button', () => {
+
+    cy.stub(store, 'createStore').returns(() => {
+      return {
+        serverConfig: { type: 'JS' },
+        onRun: cy.spy().as('onRun'),
+      };
+    });
+
+    mountRunModal();
+
+    getEl('run-modal-button').should('have.text', 'Run');
+    getEl('run-modal-button').click();
+    cy.get('@onRun').should('have.been.calledOnce');
+  });
+})
+
