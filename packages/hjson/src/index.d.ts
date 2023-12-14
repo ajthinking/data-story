bbec434d6b406f2b87fb59ffde4817440446dc1d
stone-lyl <stone-lyl@outlook.com>
2023-12-14 21:53:29 +0800

    chore: add hjson's .d.ts

--- packages/hjson/src/index.d.ts
+++ packages/hjson/src/index.d.ts
@@ -1,5 +1,11 @@
-// hjson.d.ts
-declare module '@data-story/hjson' {
-  const hjson: any;
-  export default hjson;
-}
+declare const _default: {
+  parse: (text: string, options?: any) => any;
+  stringify: (value: any, options?: any) => string;
+  endOfLine: () => string;
+  setEndOfLine: (eol: string) => void;
+  version: string;
+  rt: any;
+  comments: any;
+  dsf: any;
+};
+export default _default;
