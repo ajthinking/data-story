70590ea6046a1f14326313ad54b04d11bdedb832
Anders Jürisoo <jurisoo@hotmail.com>
2023-12-20 09:32:09 +0100

    Prepare hubspot package

---
+++ packages/hubspot/src/hubspotProvider.ts
@@ -0,0 +1,22 @@
+import { Application, ComputerConfig, ComputerFactory, ServiceProvider } from '@data-story/core';
+import * as computerConfigs from './computers'
+
+export const hubspotProvider: ServiceProvider = {
+  register: (app: Application) => {
+    const configs = Object.values(computerConfigs as {[key: string]: ComputerConfig})
+    app.addComputers(configs);
+  },
+  
+  boot: (app: Application) => {}
+}
+
+// Contacts.getAll
+
+// Contacts.getById
+// Contacts.create
+// Contacts.update
+// Contacts.archive
+
+// Contacts.batch.create
+// Contacts.batch.update
+// Contacts.batch.archive
