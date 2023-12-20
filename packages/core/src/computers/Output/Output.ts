e9bd1b097ecc8c061d04f8c1dd768e3262bde404
Anders Jürisoo <jurisoo@hotmail.com>
2023-12-20 19:52:51 +0100

    remove exclamation marks on input.pull

--- packages/core/src/computers/Output/Output.ts
+++ packages/core/src/computers/Output/Output.ts
@@ -7,7 +7,7 @@ export const Output: ComputerConfig = {
   
   async *run({ input, output }) {
     while(true) {
-      const incoming = input!.pull()
+      const incoming = input.pull()
       console.log('PUSHING FROM THE OUTPUT NODE!')
       output.push(incoming)
 
