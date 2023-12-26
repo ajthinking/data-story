b2fe75094d3f1e623d9a8fedfe9409efd0b5ce82
Anders Jürisoo <jurisoo@hotmail.com>
2023-12-26 11:40:04 +0100

    Discountinue ServerClient.describe in favor of init

--- packages/ui/src/components/DataStory/clients/ServerClient.ts
+++ packages/ui/src/components/DataStory/clients/ServerClient.ts
@@ -3,7 +3,6 @@ import { SerializedReactFlow } from '../../../SerializedReactFlow';
 
 export interface ServerClient {
   init: () => void;
-  describe: () => void;
   run: (diagram: Diagram) => void;
   save: (name: string, reactFlow: SerializedReactFlow) => {}
 }
