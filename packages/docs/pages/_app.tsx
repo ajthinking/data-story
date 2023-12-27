448c5a8ae803f3ade4cc4147d37cc04ac12b79bb
stone-lyl <stone-lyl@outlook.com>
2023-12-27 21:31:54 +0800

    feat: Data-Story PWA has been installed successfully

--- packages/docs/pages/_app.tsx
+++ packages/docs/pages/_app.tsx
@@ -1,11 +1,18 @@
 import '../globals.css'
 import Head from 'next/head';
+import icons from '../public/icons.json';
 
 export default function extra({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>data-story</title>
+        // We will generate corresponding links in the Head for all images in the icons folder
+        {
+          icons.icons.map(({src, sizes}) => (
+            <link key={src} rel="icon" href={`/${src}`} sizes={sizes} />
+          ))
+        }
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <link rel='pwa-icon' href='/favicon.png'/>
         <link rel='manifest' href='/manifest.json' />
