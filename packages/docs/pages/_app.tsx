a3c8622bdd5b9fd976a9ab67cab66eb5412265fe
stone-lyl <stone-lyl@outlook.com>
2023-12-27 17:45:19 +0800

    feat: The mainfest.json file has been loaded successfully

--- packages/docs/pages/_app.tsx
+++ packages/docs/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../globals.css'
+import Head from 'next/head';
 
-export default function Nextra({ Component, pageProps }) {
+export default function extra({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>data-story</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <link rel='pwa-icon' href='/favicon.png'/>
+        <link rel='manifest' href='/manifest.json' />
+      </Head>
       <Component {...pageProps} />
     </>
   )
