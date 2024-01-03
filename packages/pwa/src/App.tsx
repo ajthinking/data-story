02d2d7f5b828bc6cc46961373086cd191ed5ac46
stone-lyl <stone-lyl@outlook.com>
2024-01-03 13:04:31 +0800

    feat: add pwa assets

--- packages/pwa/src/App.tsx
+++ packages/pwa/src/App.tsx
@@ -1,13 +1,13 @@
-import * as UI from '@data-story/ui'
-import * as Core from '@data-story/core';
+import { DataStory } from '@data-story/ui'
+import { coreNodeProvider, Application } from '@data-story/core';
 function App({ mode}: { mode?: 'js' | 'node' }) {
-  const app = new Core.Application()
-    .register(Core.coreNodeProvider)
+  const app = new Application()
+    .register(coreNodeProvider)
     .boot();
 
   return (
     <div style={{ height: '100vh', width: '100vw', overflow: 'hidden' }} data-cy="playground">
-      <UI.DataStory
+      <DataStory
         server={mode === 'node'
           ? { type: 'SOCKET', url: 'ws://localhost:3100' }
           : { type: 'JS', app }}
