d9afd76c66714b0aeb164ba4afb005b690f1aa73
stone-lyl <stone-lyl@outlook.com>
2024-01-03 21:52:31 +0800

    feat: config pwa in pwa project

--- packages/pwa/vite.config.ts
+++ packages/pwa/vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { type ManifestOptions, VitePWA } from 'vite-plugin-pwa'
+import manifest from './public/manifest.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    strategies: 'injectManifest',
+    srcDir: 'src',
+    filename: 'sw.ts',
+    workbox: {
+      clientsClaim: true,
+      skipWaiting: true
+    },
+    includeAssets: ['./public/favicon.ico', './public/robots.txt', './public/manifest.json'],
+    manifest: manifest as ManifestOptions
+  })],
   optimizeDeps: {
     include: ["@data-story/core", "@data-story/hubspot", "@data-story/nodejs", "@data-story/openai", "@data-story/ui"]
   },
