ddbaed4dd97a70266a9d7dde7ede9650850bf304
stone-lyl <stone-lyl@outlook.com>
2024-01-03 11:30:08 +0800

    fix: fix require is undefined issue

--- packages/pwa/vite.config.ts
+++ packages/pwa/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["@data-story/core", "@data-story/hubspot", "@data-story/nodejs", "@data-story/openai", "@data-story/ui"]
+  },
 })
