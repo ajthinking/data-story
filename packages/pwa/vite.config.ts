7a1557ae63c6026fb4cc733dbf4c5e4763a3c450
stone-lyl <stone-lyl@outlook.com>
2024-01-04 10:50:12 +0800

    feat: add manifest and the generated service worker on application entry point in dev

--- packages/pwa/vite.config.ts
+++ packages/pwa/vite.config.ts
@@ -1,21 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { type ManifestOptions, VitePWA } from 'vite-plugin-pwa'
+import { VitePWA, type ManifestOptions } from 'vite-plugin-pwa'
 import manifest from './public/manifest.json';
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
-    srcDir: 'src',
-    workbox: {
-      clientsClaim: true,
-      skipWaiting: true,
-    },
-    includeAssets: ['./public/favicon.ico', './public/robots.txt'],
-    manifest: manifest as ManifestOptions
-  })],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+      includeAssets: ['./public/favicon.ico', './public/robots.txt'],
+      manifest: manifest as ManifestOptions
+    })
+  ],
   optimizeDeps: {
     include: ["@data-story/core", "@data-story/hubspot", "@data-story/nodejs", "@data-story/openai", "@data-story/ui"]
   },
