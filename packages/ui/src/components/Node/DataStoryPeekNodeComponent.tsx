cecda53ab45e5ad1894764ee7f78a4edcdc6346b
Anders Jürisoo <jurisoo@hotmail.com>
2024-01-06 10:46:48 +0100

    Smaller text in Peek, better connection

--- packages/ui/src/components/Node/DataStoryPeekNodeComponent.tsx
+++ packages/ui/src/components/Node/DataStoryPeekNodeComponent.tsx
@@ -3,9 +3,7 @@ import { StoreSchema, useStore } from '../DataStory/store/store';
 import { shallow } from 'zustand/shallow';
 import { DataStoryNodeData } from './DataStoryNode';
 import MarkdownIt from 'markdown-it';
-import CustomHandle from './CustomHandle';
-import { PortIcon } from '../DataStory/icons/portIcon';
-import { Handle, NodeResizer, Position } from 'reactflow';
+import { Handle, Position } from 'reactflow';
 
 const markdown = new MarkdownIt();
 
@@ -29,31 +27,38 @@ const DataStoryPeekNodeComponent = ({ id, data, selected }: {
   return (
     (
       <div
-        className="shadow-xl bg-gray-50 rounded p-4 border border-gray-300 overflow-hidden"
+        className="shadow-xl bg-gray-50 rounded border border-gray-300 overflow-hidden"
         onClick={() => {
           if(minimized) setMinimized(false);
         }}
         onDoubleClick={() => setMinimized(!minimized)}
       >
-        <div className="absolute mx-1 my-4 z-30">
-          <Handle
-            className="relative"
-            type="target"
-            position={Position.Left}
-            style={{ opacity: 0, backgroundColor: 'red', position: 'relative', height: 12, width: 12, top: 0, right: 0}}
-            id={input.id}
-            isConnectable={true}
-          />
+        <div className="absolute z-30">
+          <div className="absolute">
+            
+          </div>
+          <div className="">
+            <Handle
+              className="relative"
+              type="target"
+              position={Position.Left}
+              style={{ opacity: 0, backgroundColor: 'red', position: 'relative', height: 12, width: 12, top: 0, right: 0}}
+              id={input.id}
+              isConnectable={true}
+            />
+          </div>
         </div>       
-        {!minimized && <div className="bg-gray-50 text-gray-600 font-mono text-xs">
+        {!minimized && <div className="p-2 bg-gray-50 text-gray-600 font-mono text-xxxs">
           <pre>
             {peek && JSON.stringify(peek, null, 2)}
             {!peek && '🕒'}
           </pre>
         </div>}
 
-        {minimized && <div className="bg-gray-50 text-gray-600 font-mono text-xs">
+        {minimized && <div className="p-2 bg-gray-50 text-gray-600 font-mono text-xxxs">
+          <pre>
           🙈
+          </pre>
         </div>}
       </div>
     )
