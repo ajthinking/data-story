82af98d97f902deedb84b935977c8fe8ce8fcb80
stone-lyl <stone-lyl@outlook.com>
2024-01-09 17:48:56 +0800

    feat: useForm control wrap fileSelect

--- packages/ui/src/components/DataStory/Form/SelectFile.tsx
+++ packages/ui/src/components/DataStory/Form/SelectFile.tsx
@@ -1,39 +1,54 @@
 import { DataStoryNode } from '../../Node/DataStoryNode';
-import { Param } from '@data-story/core';
-import { UseFormReturn } from 'react-hook-form';
-import React, { useState } from 'react';
+import { Param, SelectButton } from '@data-story/core';
+import { Controller, UseFormReturn } from 'react-hook-form';
+import React, { useEffect, useState } from 'react';
 
-// 1. 使用 tailwindcss 的样式 优化 selectFile 的样式
-// 2. 优化 input 的样式
 interface FileSystemFileHandle {
   getFile: () => Promise<File>;
 }
 
-export function SelectFile({ param, form, node }: {
+export function SelectFile({ param, form, node, inputMode }: {
   param: Param,
   form: UseFormReturn<{
     [x: string]: any;
   }, any>,
   node: DataStoryNode,
+  inputMode: SelectButton,
 }) {
-  console.log('param', param);
-  console.log('form', form);
-  console.log('node', node);
+
+  return (
+    <Controller
+      name={`params.${param.name}`}
+      control={form.control}
+      render={({ field }) => <FileSelect {...field} />}
+    />
+  );
+}
+
+const FileSelect = ({onChange, value, name, disabled} : {
+  onChange: 	(value: FileSystemFileHandle) => void
+  value: FileSystemFileHandle,
+  disabled?: boolean,
+  name: string,
+}) => {
 
   const [fileContent, setFileContent] = useState<string>('');
-  const [isFilePicked, setIsFilePicked] = useState<boolean>(false);
+  const [fileName, setFileName] = useState<string>('');
 
-  const handleFileRead = async (fileHandle: FileSystemFileHandle) => {
-    const file = await fileHandle.getFile();
-    const content = await file.text();
-    console.log(file, 'file');
-    console.log(content, 'content');
-    setFileContent(content);
-    setIsFilePicked(true); // 文件已选，可以禁用按钮
+  useEffect(() => {
+    void handleFileRead(value);
+  }, [value]);
 
+  const handleFileRead = async(fileHandle: FileSystemFileHandle) => {
+    if(fileHandle instanceof FileSystemFileHandle ){
+      const file = await fileHandle.getFile();
+      const content = await file.text();
+      setFileName(file.name);
+      setFileContent(content);
+    }
   };
 
-  const handleFileSelect = async () => {
+  const handleFileSelect = async() => {
     try {
       const [fileHandle] = await window.showOpenFilePicker({
         types: [
@@ -47,27 +62,27 @@ export function SelectFile({ param, form, node }: {
         ],
       });
 
-      await handleFileRead(fileHandle as unknown as FileSystemFileHandle);
-    } catch (error) {
+      onChange(fileHandle);
+    } catch(error) {
       console.error('File selection was cancelled or failed', error);
     }
   };
-
   return (
     <div>
       <button
         onClick={handleFileSelect}
-        disabled={isFilePicked}
+        name={name}
+        disabled={disabled}
         className={`${
-          isFilePicked ? 'cursor-not-allowed opacity-50' : 'hover:bg-blue-700'
+          disabled ? 'cursor-not-allowed opacity-50' : 'hover:bg-blue-700'
         } bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
       >
-        Select a CSV or JSON file13111
+        Select a CSV or JSON file
       </button>
-      <div className="w-72 h-48 overflow-auto">
+      <p className={'p-3'}> {fileName} </p>
+      <pre className="h-48 p-3 overflow-auto">
         {fileContent}
-      </div>
+      </pre>
     </div>
   );
 }
-
