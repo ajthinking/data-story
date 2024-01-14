4632b85874bd3d4a0422a67224fb43a2ed2a2627
stone-lyl <stone-lyl@outlook.com>
2024-01-14 13:41:33 +0800

    feat: add saveDiagramToJSON and loadDiagramFromJSON method

--- packages/ui/src/components/DataStory/clients/ServerClient.ts
+++ packages/ui/src/components/DataStory/clients/ServerClient.ts
@@ -1,7 +1,34 @@
-import { Diagram } from '@data-story/core';
+import { Diagram, NodeDescription } from '@data-story/core';
 
 export interface ServerClient {
   init: () => void;
   run: (diagram: Diagram) => void;
   save: (name: string, diagram: Diagram) => {}
+}
+
+export type ConstructorParams = {
+  setAvailableNodes: (nodes: NodeDescription[]) => void;
+  updateEdgeCounts: (edgeCounts: Record<string, number>) => void;
+  setPeek: (key: string, peek: any) => void;
+  setNodes: (nodes: any) => void;
+  setEdges: (edges: any) => void;
+};
+
+export const saveDiagramToJSON = (name: string, diagram: Diagram) => {
+
+  return JSON.stringify({
+    type: 'save',
+    name,
+    diagram
+  });
+};
+
+export const loadDiagramFromJSON = (json: string) => {
+  const { name, diagram } = JSON.parse(json);
+
+  return {
+    type: 'load',
+    name,
+    diagram
+  };
 }
