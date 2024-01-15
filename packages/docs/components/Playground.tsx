3686c1a122e07cf87805869e44ff8dd9ace25720
stone-lyl <stone-lyl@outlook.com>
2024-01-15 17:21:01 +0800

    feat: implement save the diagram to localStorage

--- packages/docs/components/Playground.tsx
+++ packages/docs/components/Playground.tsx
@@ -1,27 +1,69 @@
 import { DataStory } from '@data-story/ui'
-import { coreNodeProvider, Application } from '@data-story/core';
+import { Application, coreNodeProvider, Diagram } from '@data-story/core';
 import React from 'react';
 
-export default ({ mode}: { mode?: 'js' | 'node' }) => {
+const saveDiagram = (key: string, diagram: Diagram) => {
+
+  const diagramJSON = JSON.stringify({
+    type: 'save',
+    name: key,
+    diagram
+  });
+
+  localStorage?.setItem(key, diagramJSON);
+};
+
+const loadDiagram = (key: string) => {
+  if (typeof window === 'undefined' || !localStorage?.getItem(key)) {
+    return {
+      type: 'load',
+      name: key,
+      diagram: null
+    };
+  }
+
+  const json = localStorage?.getItem(key);
+  const { name, diagram } = JSON.parse(json);
+
+  return {
+    type: 'load',
+    name,
+    diagram
+  };
+}
+
+const LocalStorageKey = 'data-story-diagram';
+
+export default ({ mode }: {mode?: 'js' | 'node'}) => {
   const app = new Application()
     .register(coreNodeProvider)
     .boot();
   const dataStoryRef = React.useRef(null);
 
+  const { diagram } = loadDiagram(LocalStorageKey);
+  const [initDiagram] = React.useState<Diagram>(diagram);
+
+  // 1. 使用 saveDiagramToJSON 进行 diagram 的保存 localStorage
+
+
+  // 2. 讨论后面的问题
+  // 3. 说明每个字段的含义和使用：initDiagram and dataStoryRef.current.toDiagram
+  // 4. 提PR
 
   return (
     <div className="w-full" style={{ height: '100vh' }} data-cy="playground">
       <button
         onClick={() => {
           const diagram = dataStoryRef.current.toDiagram();
-          console.log('diagram', diagram)
+          saveDiagram(LocalStorageKey, diagram);
         }}
         style={{ zIndex: 1000, top: 200 }}
         className="fixed left-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full shadow-lg inline-block">
-        get diagram
+        save diagram
       </button>
       <DataStory
         ref={dataStoryRef}
+        initDiagram={initDiagram}
         server={mode === 'node'
           ? { type: 'SOCKET', url: 'ws://localhost:3100' }
           : { type: 'JS', app }}
--- packages/ui/src/components/DataStory/clients/ServerClient.ts
+++ packages/ui/src/components/DataStory/clients/ServerClient.ts
@@ -1,26 +1,7 @@
-import { Diagram, NodeDescription } from '@data-story/core';
+import { Diagram } from '@data-story/core';
 
 export interface ServerClient {
   init: () => void;
   run: (diagram: Diagram) => void;
   save: (name: string, diagram: Diagram) => {}
-}
-
-export const saveDiagramToJSON = (name: string, diagram: Diagram) => {
-
-  return JSON.stringify({
-    type: 'save',
-    name,
-    diagram
-  });
-};
-
-export const loadDiagramFromJSON = (json: string) => {
-  const { name, diagram } = JSON.parse(json);
-
-  return {
-    type: 'load',
-    name,
-    diagram
-  };
 }
