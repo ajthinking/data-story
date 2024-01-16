74b1ab593334a4ca5eee939d65630d1f8531077a
stone-lyl <stone-lyl@outlook.com>
2024-01-16 14:42:17 +0800

    feat: change button to ControlButton

--- packages/docs/components/Playground.tsx
+++ packages/docs/components/Playground.tsx
@@ -1,6 +1,7 @@
 import { Application, coreNodeProvider, Diagram } from '@data-story/core';
 import React from 'react';
-import { DataStory } from '@data-story/ui';
+import { DataStory, SaveIcon } from '@data-story/ui';
+import { ControlButton } from 'reactflow';
 
 const saveDiagram = (key: string, diagram: Diagram) => {
 
@@ -45,26 +46,23 @@ export default ({ mode }: {mode?: 'js' | 'node'}) => {
 
   const SaveComponent = () => {
     return (
-      <button
+      <ControlButton
+        title="Save"
+        aria-label="Save"
         onClick={() => {
           const diagram = dataStoryRef.current.toDiagram();
           saveDiagram(LocalStorageKey, diagram);
-        }}
-        style={{ zIndex: 1000, top: 200 }}
-        className="fixed bg-blue-300 hover:bg-blue-400 text-black py-1 px-2 text-sm rounded inline-flex items-center">
-        <svg className="fill-current w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-          <path
-            d="M17.707 7.293l-5-5A.999.999 0 0 0 12 2H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8a.999.999 0 0 0-.293-.707zM12 3.414L15.586 7H12V3.414zM16 16H4V4h7v5h5v7z"/>
-        </svg>
-        <span>Save</span>
-      </button>
+          window.alert('Diagram saved!');
+        }}>
+        <SaveIcon />
+      </ControlButton>
     );
   }
 
   return (
     <div className="w-full" style={{ height: '100vh' }} data-cy="playground">
       <DataStory
-        slotComponent={<SaveComponent />}
+        slotComponent={<SaveComponent/>}
         ref={dataStoryRef}
         initDiagram={initDiagram}
         server={mode === 'node'
