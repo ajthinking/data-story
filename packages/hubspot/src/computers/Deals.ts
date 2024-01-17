062c54dbfa5590e094bb8857f20d981c032881b6
Anders Jürisoo <jurisoo@hotmail.com>
2024-01-17 11:56:58 +0100

    Add HubSpot nodes

--- packages/hubspot/src/computers/Deals.ts
+++ packages/hubspot/src/computers/Deals.ts
@@ -1,78 +1,15 @@
-import { ComputerConfig, num, str } from '@data-story/core';
-import { Client } from '@hubspot/api-client';
-import { EntityPage } from '../types/EntityPage';
-import { CrmEntity } from '../types/CrmEntity';
+import { archive } from '../crm/entity/archive';
+import { batchCreate } from '../crm/entity/batchCreate';
+import { batchUpdate } from '../crm/entity/batchUpdate';
+import { batchArchive } from '../crm/entity/batchArchive';
+import { create } from '../crm/entity/create';
+import { getAll } from '../crm/entity/getAll';
+import { update } from '../crm/entity/update';
 
-export const Deals: ComputerConfig = {
-  name: 'Deals',
-  label: 'Deals',
-  inputs: [],
-  outputs: ['all', 'errors'],
-  category: 'Hubspot',
-  params: [
-    str({
-      name: 'entity',
-      label: 'Entity',
-      help: 'The entity to retrieve.',
-      value: 'deals',
-    }),
-    str({
-      name: 'properties',
-      label: 'Properties',
-      help: 'Comma separated list of properties.',
-      value: '',
-    }),     
-    num({
-      name: 'limit',
-      label: 'Limit',
-      help: 'The maximum number of deals to return.',
-      value: String('300'),
-    }),
-  ],
-  async *run({ output, params }) {
-    const hubspot = new Client({
-      accessToken: process.env.HUBSPOT_PRIVATE_APP_TOKEN,
-      numberOfApiCallRetries: 3,
-    })
-
-    let taken = 0
-
-    const limit = params.limit
-      ? Number(params.limit)
-      : Infinity
-    const entity = params.entity as CrmEntity
-    const properties: string[] = (params.properties as string)
-      .split(',')
-      .map((p) => p.trim())
-    let nextPage = null;
-    
-    do {
-      // Make the API call to get a page of results
-      try {
-        let page: EntityPage = await hubspot.crm[entity].basicApi.getPage(
-          100,
-          nextPage?.after,
-          properties,
-        );
-
-        // Output the results
-        taken += page.results.length
-        output.pushTo('all', page.results);
-      
-        // Check if there is a next page of results
-        nextPage = page.paging?.next;        
-      } catch(e: any) {
-        console.log(e)
-        output.pushTo('errors', [{
-          message: e.message
-        }])
-
-        return
-      }
-
-      yield;
-    } while (nextPage && taken < limit);
-
-    yield;
-  },
-}
+export const Deals = getAll('Deals')
+export const DealsCreate = create('Deals')
+export const DealsBatchCreate = batchCreate('Deals')
+export const DealUpdate = update('Deal')
+export const DealsBatchUpdate = batchUpdate('Deals')
+export const DealsArchive = archive('Deals')
+export const DealsBatchArchive = batchArchive('Deals')
---
+++ packages/hubspot/src/crm/entity/batchCreate.ts
@@ -0,0 +1,60 @@
+import { ComputerConfig, num, str } from '@data-story/core';
+import { Client } from '@hubspot/api-client';
+import { EntityPage } from './EntityPage';
+import { CrmEntityName } from './CrmEntityName';
+
+const Template: ComputerConfig = {
+  name: 'NAME',
+  label: 'LABEL',
+  inputs: ['input'],
+  outputs: ['updated', 'errors'],
+  category: 'Hubspot',
+  params: [
+    str({
+      name: 'entity',
+      label: 'Entity',
+      help: 'The entity to retrieve.',
+      value: 'ENTITY',
+    }),
+    str({
+      name: 'properties',
+      label: 'Properties',
+      help: 'Comma separated list of properties.',
+      value: '',
+    }),     
+    num({
+      name: 'limit',
+      label: 'Limit',
+      help: 'The maximum number of companies to return.',
+      value: String('300'),
+    }),
+  ],
+  async *run({ input, output, params }) {
+    while(true) {
+      const [ incoming ] = input.pull(1)
+
+      // sleep 1 second
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      output.push([incoming])
+
+      yield;
+    }
+  },
+}
+
+export const batchCreate = (name: string) => {
+  const { run, ...templateProperties } = Template
+
+  const config = {
+    ...structuredClone(templateProperties),
+    name: `${name}BatchCreate`,
+    label: `${name}.batchCreate`,
+    run,
+  }
+  
+  const entityParam = config.params!.find((p) => p.name === 'entity')
+  entityParam!.inputMode.value = name.toLowerCase()
+
+  return config
+}
