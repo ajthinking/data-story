e97dc19672a949843380eac3fcd41b26805d411c
stone-lyl <stone-lyl@outlook.com>
2024-02-02 17:13:22 +0800

    build: add esm config export in ui

--- packages/ui/webpack.config.js
+++ packages/ui/webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const deps = require('./package.json').dependencies;
 
-module.exports = {
+console.log(Object.keys(deps), '1111111111111');
+const commonJSConfig = {
   devtool: 'source-map',
   mode: 'development',
   entry: './src/index.ts',
@@ -33,3 +34,46 @@ module.exports = {
     extensions: [ '.js', '.jsx', '.ts', '.tsx' ]
   }
 };
+
+const esmConfig = {
+  devtool: 'source-map',
+  entry: './src/index.ts',
+  mode: 'development',
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'bundle.mjs',
+    libraryTarget: 'module',
+    clean: true,
+    library: {
+      type: 'module',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      }
+    ]
+  },
+  externals: [
+    'react',
+    'react-dom',
+    'react/jsx-runtime',
+    ...Object.keys(deps)
+  ],
+  resolve: {
+    extensions: [ '.js', '.jsx', '.ts', '.tsx' ]
+  },
+  externalsType: 'module',
+  experiments: {
+    outputModule: true,
+  },
+};
+
+module.exports = [commonJSConfig, esmConfig];
