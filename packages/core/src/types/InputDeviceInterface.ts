ec9b732346c69556b5dae1ae8b24934b483741c8
Anders Jürisoo <jurisoo@hotmail.com>
2024-02-04 07:41:40 +0100

    Add pullNew

--- packages/core/src/types/InputDeviceInterface.ts
+++ packages/core/src/types/InputDeviceInterface.ts
@@ -5,6 +5,7 @@ import { LinkId } from '../types/Link';
 export interface InputDeviceInterface {
   pull: (count?: number) => ItemWithParams[]
   pullFrom: (name: string, count?: number) => ItemWithParams[]
+  pullNew: (template?: ItemValue) => ItemWithParams[]
   havePort(name: string): boolean;
   haveItemsAtInput(name: string, minimum?: number): boolean;
   haveAllItemsAtInput(name: string): boolean;
