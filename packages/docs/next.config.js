9bc7abe707ef0c53f3f15a3892a9469fbef3eb49
stone-lyl <stone-lyl@outlook.com>
2024-02-07 14:34:42 +0800

    Revert "fix: Minified React error #130"
    This reverts commit fb889b475700278baf108dcd6a21d40d74b18c6a.

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -10,6 +10,7 @@ module.exports = {
   images: {
     unoptimized: true,
   },
+  experimental: { esmExternals: false },
   webpack: (config, context) => {
     const baseConfig = nextraConfig.webpack(config, context);
 
@@ -19,31 +20,10 @@ module.exports = {
       baseConfig.module.rules.push({
         test: /\.js$/,
         enforce: 'pre',
-        use: [ 'source-map-loader' ],
+        use: ['source-map-loader'],
       });
-
-      baseConfig.ignoreWarnings = [ /Failed to parse source map/ ];
+      baseConfig.ignoreWarnings = [/Failed to parse source map/];
     }
-    baseConfig.resolve.alias = {
-      ...baseConfig.resolve.alias,
-      'react/jsx-runtime.js': 'react/jsx-runtime',
-      'react/jsx-dev-runtime.js': 'react/jsx-dev-runtime',
-    };
-
-    baseConfig.module.rules.push({
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-          plugins: [
-            ['@babel/plugin-transform-react-jsx', { jsxRuntime: 'classic', }]
-          ]
-        }
-      }
-    });
-    baseConfig.resolve.fallback = { fs: false };
 
     return baseConfig;
   },
