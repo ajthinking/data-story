c41ab804ae3de904177d0010298fd93c9761eb9c
Anders Jürisoo <jurisoo@hotmail.com>
2024-02-28 19:32:08 +0100

    Use factories for format conversion

---
+++ packages/ui/src/factories/ReactFlowFactory.ts
@@ -0,0 +1,45 @@
+import { Diagram } from '@data-story/core'
+import { ReactFlowJsonObject } from 'reactflow'
+
+export const ReactFlowFactory = {
+  fromDiagram(diagram: Diagram): ReactFlowJsonObject<any, any> {
+    return {
+      nodes: diagram.nodes.map(node => {
+        return {
+          width: 128,
+          height: 52,
+          id: node.id,
+          position: {
+            x: node.position!.x,
+            y: node.position!.y
+          },
+          data: {
+            docs: node.docs,
+            params: node.params,
+            computer: node.type,
+            label: (node?.label || node.type) as string,
+            inputs: node.inputs,
+            outputs: node.outputs,
+          },
+          type: (() => {
+            if (node.type === 'Comment') return 'dataStoryCommentNodeComponent';
+            if (node.type === 'Input') return 'dataStoryInputNodeComponent';
+            if (node.type === 'Output') return 'dataStoryOutputNodeComponent';
+
+            return 'dataStoryNodeComponent'
+          })(),
+        }
+      }),
+      edges: diagram.links.map(link => {
+        return {
+          sourceHandle: link.sourcePortId,
+          targetHandle: link.targetPortId,
+          source: diagram.nodes.find(node => node.outputs.find(output => output.id === link.sourcePortId) !== undefined)!.id,
+          target: diagram.nodes.find(node => node.inputs.find(input => input.id === link.targetPortId) !== undefined)!.id,
+          id: link.id,
+        }
+      }),
+      viewport: diagram.viewport,
+    }
+  }
+}
