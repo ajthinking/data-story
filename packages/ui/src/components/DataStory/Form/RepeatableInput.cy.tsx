c4ca9dd4910a6f5d13f3b037df7d8ac4a3cc75a8
stone-lyl <stone-lyl@outlook.com>
2024-03-09 17:19:56 +0800

    test: add the methods for adding, deleting, and reordering rows in the RepeatableInput test

--- packages/ui/src/components/DataStory/Form/RepeatableInput.cy.tsx
+++ packages/ui/src/components/DataStory/Form/RepeatableInput.cy.tsx
@@ -1,15 +1,13 @@
-// mock PortSelectionParam and StringableParam component in RepeatableInput.tsx
 import { DataStoryProvider } from '../store/store';
-// import { RunModal } from '../modals/runModal';
 import React from 'react';
-// import { NodeSettingsModal } from '../modals/nodeSettingsModal/nodeSettingsModal';
 import { ReactFlowProvider } from 'reactflow';
 import { RepeatableInput } from './RepeatableInput';
-import { useForm } from 'react-hook-form';
+import { useFieldArray, useForm } from 'react-hook-form';
 import { RepeatableInputProps } from '../types';
 import { ParamsComponentFactory } from '../modals/nodeSettingsModal/tabs/Params/ParamsComponentFactory';
 import { defaultValues, mockNode, mockParam } from './mocks';
 
+let globalSwap: (draggedRowIndex: number, targetRowIndex: number) => void;
 const RepeatableInputWithForm = () => {
   const mockForm = useForm({
     defaultValues
@@ -19,34 +17,74 @@ const RepeatableInputWithForm = () => {
   ParamsComponentFactory.defaultInstance.availableComponents = [
     {
       getComponent: (params) => {
-        console.log('params', params);
-        return <div data-cy='data-story-stringable-param'>{params.name}</div>
+        const value = params.form.getValues(params.name as string);
+        return <div data-cy='data-story-stringable-param'> {value} </div>
       },
       getType: () => 'StringableParam'
     },
     {
       getComponent: (params) => {
-        return <div data-cy='data-story-port-selection-param'>{params.name}</div>
+        const value = params.form.getValues(params.name as string);
+        return <div data-cy='data-story-port-selection-param'>{value}</div>
       },
       getType: () => 'PortSelectionParam'
     }
   ];
 
+  const { swap } = useFieldArray({
+    control: mockForm.control,
+    name: `params.${mockParam.name}`,
+  });
+
+  globalSwap = swap;
+
   return (<DataStoryProvider>
     <ReactFlowProvider>
-      <RepeatableInput form={mockForm} node={mockNode} param={mockParam} />
+      <RepeatableInput form={mockForm} node={mockNode} param={mockParam}/>
     </ReactFlowProvider>
   </DataStoryProvider>)
 }
 
 function mountRepeatableInput() {
-  cy.mount(<RepeatableInputWithForm />);
+  cy.mount(<RepeatableInputWithForm/>);
 }
 
 describe('<RepeatableInput />', () => {
+
+  beforeEach(() => {
+    mountRepeatableInput();
+  });
+
   it('renders', () => {
 
-    mountRepeatableInput();
     cy.dataCy('data-story-repeatable-input').should('exist');
   });
+
+  it('add new row', () => {
+
+    cy.dataCy('data-story-repeatable-add-row').should('exist');
+    cy.dataCy('data-story-repeatable-row').should('have.length', 2);
+    cy.dataCy('data-story-repeatable-add-row').click();
+    cy.dataCy('data-story-repeatable-row').should('have.length', 3);
+  });
+
+  it('delete row', () => {
+
+    cy.dataCy('data-story-repeatable-delete-row').should('exist');
+    cy.dataCy('data-story-repeatable-row').should('have.length', 2);
+    cy.dataCy('data-story-repeatable-delete-row').first().click();
+    cy.dataCy('data-story-repeatable-row').should('have.length', 1);
+
+  })
+  it('reorder row', () => {
+
+    cy.dataCy('data-story-repeatable-drag-row').should('exist');
+    cy.dataCy('data-story-repeatable-row').should('have.length', 2);
+    cy.dataCy('data-story-repeatable-row').first().contains('id444');
+
+    // exchange first and second row
+    globalSwap(1, 0);
+
+    cy.dataCy('data-story-repeatable-row').first().contains('id333');
+  })
 })
