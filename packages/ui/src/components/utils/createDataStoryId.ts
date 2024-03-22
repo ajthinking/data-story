b5a61431bcd9e0cef21220b1869c04b08ba930d0
stone-lyl <stone-lyl@outlook.com>
2024-03-22 17:10:54 +0800

    feat: add the nanoid for creating DataStoryId

---
+++ packages/ui/src/components/utils/createDataStoryId.ts
@@ -0,0 +1,4 @@
+import { customAlphabet } from 'nanoid';
+
+const nanoid = customAlphabet('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', 10)
+export const createDataStoryId = (): string => nanoid();
---
+++ packages/ui/src/components/utils/index.ts
@@ -0,0 +1,1 @@
+export { createDataStoryId } from './createDataStoryId';
