392e154210c83076bf99bdb75c68c7e6a369097f
stone-lyl <stone-lyl@outlook.com>
2024-03-24 23:13:16 +0800

    refact: remove createDataStoryId from ui to core

--- packages/core/src/utils/createDataStoryId.ts
+++ packages/core/src/utils/createDataStoryId.ts
@@ -1,4 +1,8 @@
 import { customAlphabet } from 'nanoid';
 
+/**
+ * 129M IDs needed, in order to have a 1% probability of at least one collision.
+ * https://zelark.github.io/nano-id-cc/
+ */
 const nanoid = customAlphabet('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', 10)
 export const createDataStoryId = (): string => nanoid();
--- packages/ui/src/components/utils/index.ts
+++
@@ -1,1 +0,0 @@
-export { createDataStoryId } from './createDataStoryId';
