c1e3ef41752c7e9b8ad8a1fa7ae33a329749539f
stone-lyl <stone-lyl@outlook.com>
2024-03-26 18:07:47 +0800

    test: add 'correctly extract headers and rows from nested array' test

--- packages/ui/src/components/Node/ItemCollection.test.ts
+++ packages/ui/src/components/Node/ItemCollection.test.ts
@@ -2,37 +2,38 @@ import { describe, it, expect } from 'vitest';
 import { ItemCollection } from './ItemCollection';
 import { nested } from './itemExamples/nested';
 
-describe('extractHeadersAndContent', () => {
-  it('should correctly extract headers and content from nested JSON data', () => {
-    const { headers, rows } = new ItemCollection([nested]).toTable();
+describe('toTable', () => {
+  it ('should correctly extract headers and rows from nested array', () => {
+    const mockData = [
+      {
+        'foo': 'bar',
+        'baz': [
+          {
+            'foo': 'bar',
+            'baz': 'qux'
+          }
+        ]
+      }
+    ];
 
+    const { headers, rows } = new ItemCollection(mockData).toTable();
     expect(headers).toMatchInlineSnapshot(`
       [
-        "objectId",
-        "properties.firstname",
-        "properties.lastname",
-        "properties.email",
-        "createdAt",
-        "updatedAt",
-        "associations.contacts",
+        "foo",
+        "baz",
       ]
     `);
     expect(rows).toMatchInlineSnapshot(`
       [
         [
-          "123456789",
-          "John",
-          "Doe",
-          "",
-          "2021-01-01T00:00:00.000Z",
-          "2021-01-01T00:00:00.000Z",
-          undefined,
+          "bar",
+          "[{\\"foo\\":\\"bar\\",\\"baz\\":\\"qux\\"}]",
         ],
       ]
     `);
   });
 
-  it('should correctly extract headers and content from 3 nested JSON data', () => {
+  it('should correctly extract headers and rows from 3 nested Object', () => {
     const jsonData = [
       {
         'foo1': 'bar1',
@@ -70,6 +71,35 @@ describe('extractHeadersAndContent', () => {
 
     expect(headers).toEqual(expectedHeaders);
     expect(rows).toEqual(expectedContent);
+  });
+
+  it('should correctly extract headers and rows from nested JSON data', () => {
+    const { headers, rows } = new ItemCollection([nested]).toTable();
+
+    expect(headers).toMatchInlineSnapshot(`
+      [
+        "objectId",
+        "properties.firstname",
+        "properties.lastname",
+        "properties.email",
+        "createdAt",
+        "updatedAt",
+        "associations.contacts",
+      ]
+    `);
+    expect(rows).toMatchInlineSnapshot(`
+      [
+        [
+          "123456789",
+          "John",
+          "Doe",
+          "",
+          "2021-01-01T00:00:00.000Z",
+          "2021-01-01T00:00:00.000Z",
+          "[{\\"id\\":\\"123456789\\",\\"type\\":\\"CONTACT_TO_COMPANY\\"}]",
+        ],
+      ]
+    `);
   });
 
   it('should handle empty array input', () => {
