5203de2d76ccd144e381cf68cf4223d86037f688
Anders Jürisoo <jurisoo@hotmail.com>
2024-03-28 13:31:53 +0100

    Re add item path in Request

--- packages/core/src/computers/Request.test.ts
+++ packages/core/src/computers/Request.test.ts
@@ -18,6 +18,27 @@ it('outputs items at data by default when using GET', async () => {
     .ok()
 })
 
+it('can output items at item_path when using GET', async () => {
+  (axios.get as any).mockResolvedValue({
+    data: {
+      crm: {
+        users: [{i: 1}, {i: 2}, {i: 3}]
+      }
+    }
+  })
+
+  await when(Request)
+    .hasParams({
+      item_path: 'crm.users'
+    })
+    .getsInput([{}])
+    .doRun()
+    .expectOutputs({
+      items: [{i: 1}, {i: 2}, {i: 3}]
+    })
+    .ok()
+})
+
 it('outputs items at data by default when using POST', async () => {
   (axios.post as any).mockResolvedValue({
     data: [{i: 1}, {i: 2}, {i: 3}]
@@ -33,18 +54,24 @@ it('outputs items at data by default when using POST', async () => {
     .ok()
 })
 
-// it.todo('can exhaust itself to a complete state', async () => {
-//   (axios.get as any).mockResolvedValue({
-//     data: [{i: 1}, {i: 2}, {i: 3}]
-//   })
+it('can output items at item_path when using POST', async () => {
+  (axios.post as any).mockResolvedValue({
+    data: {
+      crm: {
+        users: [{i: 1}, {i: 2}, {i: 3}]
+      }
+    }
+  })
 
-//   await when(Request)
-//     .hasDefaultParams()
-//     .doRun()
-//     .expectOutputs({
-//       items: [{i: 1}, {i: 2}, {i: 3}]
-//     })
-//     .doRun()
-//     .expectDone()
-//     .ok()
-// })
+  await when(Request)
+    .hasParams({
+      method: 'POST',
+      item_path: 'crm.users'
+    })
+    .getsInput([{}])
+    .doRun()
+    .expectOutputs({
+      items: [{i: 1}, {i: 2}, {i: 3}]
+    })
+    .ok()
+})
