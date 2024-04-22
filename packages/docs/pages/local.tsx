c878b2077a7600262f913104de577f4ff556549e
stone-lyl <stone-lyl@outlook.com>
2024-04-22 11:07:08 +0800

    feat: automatically find an available port for yarn dev

--- packages/docs/pages/local.tsx
+++ packages/docs/pages/local.tsx
@@ -2,6 +2,6 @@ import { DataStory } from '@data-story/ui';
 
 export default function Home() {
   return <div className="w-full" style={{ height: '100vh' }}>
-    <DataStory server={{type: 'SOCKET', url: 'http://localhost:3000'}} />
+    <DataStory server={{type: 'SOCKET', url: 'http://localhost:3100'}} />
   </div>
 }
