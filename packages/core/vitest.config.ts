40286729d14f118964c8db9cce437d6941575860
Anders Jürisoo <jurisoo@hotmail.com>
2024-05-11 11:27:40 +0200

    Drop coverage

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -3,14 +3,15 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    coverage: {
-      all: true,
-      exclude: [
-        './**/*.test.ts',
-        './dist/**',
-        './src/types/**',
-        './src/support/computerTester/**'
-      ]
-    },
+    // Dropped because of CJS is deprecated
+    // coverage: {
+    //   all: true,
+    //   exclude: [
+    //     './**/*.test.ts',
+    //     './dist/**',
+    //     './src/types/**',
+    //     './src/support/computerTester/**'
+    //   ]
+    // },
   },
 })
--- packages/openai/vitest.config.ts
+++ packages/openai/vitest.config.ts
@@ -3,12 +3,13 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    coverage: {
-      all: true,
-      exclude: [
-        './**/*.test.ts',
-        './dist/**',
-      ]
-    },
+    // Dropped because of CJS is deprecated
+    // coverage: {
+    //   all: true,
+    //   exclude: [
+    //     './**/*.test.ts',
+    //     './dist/**',
+    //   ]
+    // },
   },
 })
