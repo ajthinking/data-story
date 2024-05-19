f505cde60e76cadd034ae394b6f7ee803812f8d3
Anders Jürisoo <jurisoo@hotmail.com>
2024-05-19 12:46:02 +0200

    Extract Fill and Define components for run modal

---
+++ packages/docs/components/demos/TinkerDemo.tsx
@@ -0,0 +1,42 @@
+import { DataStory } from '@data-story/ui'
+import {
+  Application,
+  DiagramBuilder,
+  coreNodeProvider,
+  nodes,
+  multiline,
+  str,
+  UnfoldedDiagramFactory,
+} from '@data-story/core';
+
+import useWindowDimensions from '../../hooks/useWindowDimensions';
+
+// This component is just a place to sketch
+export default () => {
+  const { Create, Table, Input, Map, Output, ConsoleLog } = nodes;
+
+  const app = new Application();
+  app.register(coreNodeProvider);
+
+  app.boot();
+
+  const diagram = new DiagramBuilder()
+    .add(Create)
+    .get()
+
+  diagram.params = [
+    str({
+      name: 'message',
+      help: 'A message to pass on into the execution.',
+    })
+  ]
+
+  return (
+    <div className="w-full h-1/2">
+      <DataStory
+        server={{ type: 'JS', app }}
+        initDiagram={diagram}
+      />
+    </div>
+  );
+};
--- packages/ui/src/components/DataStory/modals/runModal/runModal.tsx
+++ packages/ui/src/components/DataStory/modals/runModal/runModal.tsx
@@ -1,25 +1,28 @@
 import { shallow } from 'zustand/shallow';
-import { Modal } from '../../modal'
+import { Modal } from '../../modal';
 import { StoreSchema, useStore } from '../../store/store';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import clsx from 'clsx';
 import { Params } from '../nodeSettingsModal/tabs';
 import { Node, Param, ParamValue, Port } from '@data-story/core';
 import { ReactFlowNode } from '../../../Node/ReactFlowNode';
 import { useForm } from 'react-hook-form';
+import FillMode from './FillMode';
+import DefineMode from './DefineMode';
 
 export interface RunModalContentProps {
-  setShowModal: (show: boolean) => void
+  setShowModal: (show: boolean) => void;
 }
 
 export const RunModalContent = (props: RunModalContentProps) => {
-  const {setShowModal}: RunModalContentProps = props;
+  const [defineMode, setDefineMode] = useState(false);
+  const { setShowModal }: RunModalContentProps = props;
 
   const runButtonReference = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     runButtonReference?.current?.focus();
-  }, [])
+  }, []);
 
   const selector = (state: StoreSchema) => ({
     onRun: state.onRun,
@@ -30,90 +33,59 @@ export const RunModalContent = (props: RunModalContentProps) => {
 
   const { onRun, serverConfig, params, setParams } = useStore(selector, shallow);
 
-  // Hack to reuse Params component
-  const nullNode = {
-    data: {
-      params,
-      inputs: [] as Port[],
-    }
-  } as ReactFlowNode;
-
-  const defaultValues = {
-    params: params.reduce((acc, param: Param) => {
-      acc[param.name] = param.value as any
-      return acc
-    }, {})
-  }
-
-  const form = useForm<{ [x: string]: any }>({
-    defaultValues,
-  });
-
-  const handleRun = () => {
-    form.handleSubmit(submitted => {
-      const newParams = params.map(param => {
-        if (!submitted.params.hasOwnProperty(param.name)) return param;
-
-        return {
-          ...param,
-          value: submitted.params[param.name]
-        };
-      });
-
-      setParams(newParams);
-      onRun();
-      setShowModal(false);
-    })();
+  const handleRun = (newParams) => {
+    setParams(newParams);
+    onRun();
+    setShowModal(false);
   };
 
-  return (<Modal
-    title={'Run'}
-    setShowModal={setShowModal}
-  >
-    <div className='flex flex-col space-y-2'  data-cy='run-modal'>
-      <div className='text-xs mb-4 text-gray-500'>Server: <span data-cy='run-modal-server' className='ml-2 font-mono text-gray-600'>{
-        (() => {
-          if(serverConfig.type === 'SOCKET') {
-            return serverConfig.url;
-          }
+  return (
+    <Modal title={'Run'} setShowModal={setShowModal}>
+      <div className="flex flex-col space-y-2" data-cy="run-modal">
+        <div className="flex flex-col space-y-2 text-xs text-gray-500">
+          {/* ***** TABS ***** */}
+          <div className="mx-4 flex space-x-8 text-xxs uppercase text-gray-400">
+            <div
+              onClick={() => setDefineMode(false)}
+              className={`pb-2 hover:text-gray-500 cursor-pointer ${!defineMode && 'border-b-2 border-blue-400'}`}
+            >
+              Fill
+            </div>
+            <div
+              onClick={() => setDefineMode(true)}
+              className={`pb-2 hover:text-gray-500 cursor-pointer ${defineMode && 'border-b-2 border-blue-400'}`}
+            >
+              Define
+            </div>
+          </div>
+        </div>
 
-          if(serverConfig.type === 'JS') {
-            return 'JS';
-          }
-
-          return 'Unknown';
-        })()
-      }</span></div>
-      <div className='flex flex-col space-y-2 text-xs mb-4 text-gray-500'>
-        <Params
-          form={form}
-          node={{ ...nullNode, }}
-        />
+        {defineMode
+          ? (<DefineMode params={params} setDefineMode={setDefineMode} />)
+          : (<FillMode params={params} setParams={setParams} handleRun={handleRun} />)}
       </div>
-      <div className='flex w-full justify-center items-center space-x-2'>
-        <button
-          ref={runButtonReference}
-          data-cy='run-modal-button'
-          className={clsx(
-            'flex w-full items-center justify-center space-y-4 mt-4 px-16 py-2',
-            'bg-blue-500 hover:bg-blue-600',
-            'font-mono text-xs text-gray-50 uppercase',
-            'rounded'
-          )}
-          onClick={handleRun}
-        >
-          Run
-        </button>
-      </div>
-    </div>
-  </Modal>)
-}
+    </Modal>
+  );
+};
 
-export const RunModal = ({ showModal, setShowModal }: {
-  showModal: boolean,
-  setShowModal: (show: boolean) => void
-}) => {
-  if(!showModal) return null;
+export const RunModal = ({ showModal, setShowModal }: { showModal: boolean; setShowModal: (show: boolean) => void }) => {
+  if (!showModal) return null;
 
-  return (<RunModalContent setShowModal={setShowModal} />)
-}
+  return <RunModalContent setShowModal={setShowModal} />;
+};
+
+{/* <div className="flex w-full justify-center items-center space-x-2">
+<button
+  ref={runButtonReference}
+  data-cy="run-modal-button"
+  className={clsx(
+    'flex w-full items-center justify-center space-y-4 mt-4 px-16 py-2',
+    'bg-blue-500 hover:bg-blue-600',
+    'font-mono text-xs text-gray-50 uppercase',
+    'rounded'
+  )}
+  onClick={() => setDefineMode(!defineMode)}
+>
+  {defineMode ? 'Fill Params' : 'Define Params'}
+</button>
+</div> */}
