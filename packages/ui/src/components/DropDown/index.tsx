3bb28bdeb7efff1e7672456c79b8ed4b19ee0e19
stone <stone-lyl@outlook.com>
2024-05-21 18:37:54 +0800

    feat: add useInteractions to control dropdown interaction

--- packages/ui/src/components/DropDown/index.tsx
+++ packages/ui/src/components/DropDown/index.tsx
@@ -1,14 +1,15 @@
 import '../../styles/globals.css';
-import { useState } from 'react';
-import { useEscapeKey } from '../DataStory/hooks/useEscapeKey';
+import { useCallback, useState } from 'react';
 import {
   autoUpdate,
   flip,
   offset,
   shift,
   useClick,
+  useDismiss,
   useFloating,
-  useInteractions
+  useInteractions,
+  useRole
 } from '@floating-ui/react';
 
 export type Option = {
@@ -31,18 +32,14 @@ export type OptionGroup = {
   selectable?: boolean
 }
 
-export const DropDown = ({ optionGroups, floatingPortalRef }: { optionGroups: OptionGroup[], floatingPortalRef: React.RefObject<HTMLElement | null>}) => {
+export const DropDown = ({ optionGroups }: {optionGroups: OptionGroup[]}) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const closeDropdown = () => {
+  const closeDropdown = useCallback(() => {
     setIsOpen(false);
-  };
+  }, []);
 
-  const {refs, floatingStyles, context} = useFloating({
+  const { refs, floatingStyles, context } = useFloating({
     open: isOpen,
     onOpenChange: setIsOpen,
     placement: 'bottom-start',
@@ -55,10 +52,15 @@ export const DropDown = ({ optionGroups, floatingPortalRef }: { optionGroups: Op
       shift()
     ]
   });
-  const click = useClick(context);
 
-  const {getReferenceProps, getFloatingProps} = useInteractions([
+  const click = useClick(context);
+  const dismiss = useDismiss(context);
+  const role = useRole(context);
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([
     click,
+    dismiss,
+    role,
   ]);
 
   return (
@@ -69,9 +71,10 @@ export const DropDown = ({ optionGroups, floatingPortalRef }: { optionGroups: Op
           {...getReferenceProps()}
           type="button"
           className="px-2 py-1 text-xs text-gray-200 group-hover:text-gray-800 focus:text-gray-800 font-medium text-xs inline-flex items-center"
-          onClick={toggleDropdown}
-        ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-3 h-3">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+            stroke="currentColor" className="w-3 h-3">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
           </svg>
         </button>
         {isOpen && (<div
@@ -82,7 +85,8 @@ export const DropDown = ({ optionGroups, floatingPortalRef }: { optionGroups: Op
           {optionGroups.map((optionGroup) => {
             return (
               <div className="mb-2" key={optionGroup.label}>
-                <div key={optionGroup.label} className="font-bold text-gray-400 flex w-full justify-center text-xs px-2 py-2 border-b uppercase tracking-widest">{optionGroup.label}</div>
+                <div key={optionGroup.label}
+                  className="font-bold text-gray-400 flex w-full justify-center text-xs px-2 py-2 border-b uppercase tracking-widest">{optionGroup.label}</div>
                 <ul role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                   {optionGroup.options.map((option) => {
                     return (
@@ -90,7 +94,7 @@ export const DropDown = ({ optionGroups, floatingPortalRef }: { optionGroups: Op
                         <div
                           className="flex justify-between px-2 py-1 text-xs text-gray-700 hover:bg-gray-100"
                           onClick={() => {
-                            if(optionGroup.selectable) {
+                            if (optionGroup.selectable) {
                               optionGroup.options.forEach((option) => {
                                 option.selected = false
                               })
@@ -99,11 +103,10 @@ export const DropDown = ({ optionGroups, floatingPortalRef }: { optionGroups: Op
                             }
 
                             option.callback({
-                              close: () => {
-                                setIsOpen(false)
-                              },
+                              close: closeDropdown,
                               selectedIndex: optionGroup.options.findIndex((option) => option.selected),
-                            })}
+                            })
+                          }
                           }
                         >
                           {option.label}
@@ -113,7 +116,8 @@ export const DropDown = ({ optionGroups, floatingPortalRef }: { optionGroups: Op
                     )
                   })}
                 </ul>
-                {optionGroup.options.length === 0 && <div className="text-xs text-gray-400 px-2 py-2">{optionGroup.emptyMessage ?? 'N/A'}</div>}
+                {optionGroup.options.length === 0 &&
+                  <div className="text-xs text-gray-400 px-2 py-2">{optionGroup.emptyMessage ?? 'N/A'}</div>}
               </div>
             )
           })}
