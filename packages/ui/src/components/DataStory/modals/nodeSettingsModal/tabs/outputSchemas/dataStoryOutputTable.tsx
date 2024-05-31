9751389d460a3f7dfaf167062a5090b6026949a4
stone <stone-lyl@outlook.com>
2024-05-31 21:53:53 +0800

    refact: use useFormContext replace props.form

--- packages/ui/src/components/DataStory/modals/nodeSettingsModal/tabs/outputSchemas/dataStoryOutputTable.tsx
+++ packages/ui/src/components/DataStory/modals/nodeSettingsModal/tabs/outputSchemas/dataStoryOutputTable.tsx
@@ -3,7 +3,7 @@ import { ColumnDef, flexRender, getCoreRowModel, Row, useReactTable } from '@tan
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { Port } from '@data-story/core';
-import { Controller, ControllerRenderProps } from 'react-hook-form';
+import { Controller, ControllerRenderProps, useFormContext } from 'react-hook-form';
 import { defaultColumns, formatOutputs, OutputSchemaProps } from './common';
 import { ReactFlowNode } from '../../../../../Node/ReactFlowNode';
 import { CloseIcon } from '../../../../icons/closeIcon';
@@ -256,10 +256,11 @@ export function OutputTable(props: {
 }
 
 export const DataStoryOutputTable = (props: OutputSchemaProps) => {
+  const { control} = useFormContext()
   return (
     <DndProvider backend={HTML5Backend}>
       <Controller
-        control={props.form.control}
+        control={control}
         name="outputs"
         render={({ field }) => (
           <OutputTable field={field} node={props.node} />
