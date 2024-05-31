28259fdbc5735f7ab10643036434f6f4fe773c15
stone <stone-lyl@outlook.com>
2024-05-31 23:14:09 +0800

    feat: improve the UseFormFieldReturn type

--- packages/ui/src/components/DataStory/Form/UseFormField.tsx
+++ packages/ui/src/components/DataStory/Form/UseFormField.tsx
@@ -1,11 +1,17 @@
-import { useFormContext, UseFormReturn } from 'react-hook-form';
+import {
+  FieldValues,
+  useFormContext,
+  UseFormReturn,
+} from 'react-hook-form';
 import { createContext, ReactNode, useContext } from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form/dist/types/form';
 
-const SubFieldContext = createContext<{fieldName: string}>({fieldName:''});
+export const SubFieldContext = createContext<{fieldName: string}>({fieldName:''});
 
 export const SubField = ({fieldName, children}: {fieldName: string, children: ReactNode}) => {
   const {fieldName: parentFieldName} = useContext(SubFieldContext);
   const value = parentFieldName ? `${parentFieldName}.${fieldName}` : fieldName;
+  // console.log(`SubField: ${value}`)
   return (
     <SubFieldContext.Provider value={{fieldName: value}}>
       {children}
@@ -13,12 +19,24 @@ export const SubField = ({fieldName, children}: {fieldName: string, children: Re
   )
 }
 
-const useFormField = () => {
+type UseFormFieldReturn<TFieldValues extends FieldValues = FieldValues, TContext = any> = Pick<
+UseFormReturn<TFieldValues, TContext>,
+'watch' | 'control'
+> & {
+  register: () => UseFormRegisterReturn;
+  setValue: (value: any) => void;
+  getValues: () => any;
+};
+
+export const useFormField = (): UseFormFieldReturn => {
   const {fieldName} = useContext(SubFieldContext);
+  // console.log(fieldName, 'fieldName')
   const form = useFormContext();
   return {
     setValue: (value: any) => form.setValue(fieldName, value),
-    value: () => form.getValues(fieldName),
-    register: () => form.register(fieldName)
+    getValues: () => form.getValues(fieldName),
+    register: () => form.register(fieldName as any),
+    watch: form.watch,
+    control: form.control,
   }
 }
