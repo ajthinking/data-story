c7281dee64dcf21bbf263c66534721ace2e20755
stone <stone-lyl@outlook.com>
2024-06-25 22:39:31 +0800

    test: enhance the linkCount test

--- packages/core/src/V2/nodes/linkCount.test.ts
+++ packages/core/src/V2/nodes/linkCount.test.ts
@@ -12,7 +12,7 @@ describe('linkCount', () => {
       expect(actual).toEqual(expected);
     });
   });
-  it('should count the number of links', () => {
+  it('should count the number of 1 the link', () => {
     testScheduler.run(({ cold, expectObservable }) => {
       const inputValues = { a: 1 };
       const inputObservable = cold('a|', inputValues);
@@ -25,6 +25,24 @@ describe('linkCount', () => {
       });
       const output$ = ports.getOutput(inputPorts).getPort('output');
       expectObservable(output$).toBe('a|', inputValues);
+    })
+  });
+
+  it('should count the number of 3 the link', () => {
+    testScheduler.run(({ cold, expectObservable }) => {
+      const inputValues = { a: 1, b: 2, c: 3 };
+      const inputObservable = cold('a - b - c|', inputValues);
+      const inputPorts = new LinkNodePorts(inputObservable, 'input');
+
+      let linkCount = 1;
+      const ports = LinkCount.boot({
+        getLinkCount: (count: number) => {
+          expect(count).toBe(linkCount++);
+        }
+      });
+
+      const output$ = ports.getOutput(inputPorts).getPort('output');
+      expectObservable(output$).toBe('a - b - c|', inputValues);
     })
   });
 
