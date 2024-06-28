61332b216f040c38f0c726d0f0e661e32e9c3cca
stone <stone-lyl@outlook.com>
2024-06-28 17:03:14 +0800

    feat: add ComposedOperator class

--- packages/core/src/V2/nodes/operator.ts
+++ packages/core/src/V2/nodes/operator.ts
@@ -1,6 +1,7 @@
 import { OperatorElement, PortProvider } from '../circuitElement';
 
 import { CreateOutputPort } from './elementPorts';
+import { composeOperators } from '../processDiagram';
 
 export class Operator implements OperatorElement {
 
@@ -11,5 +12,17 @@ export class Operator implements OperatorElement {
 
   getOutput(inputs: PortProvider): PortProvider {
     return this.createOutputPort(inputs);
+  }
+}
+
+export class ComposedOperator implements OperatorElement {
+
+  elementType = 'operator' as const;
+
+  constructor(private operators: OperatorElement[], public elementName: string) {
+  }
+
+  getOutput(inputs: PortProvider): PortProvider {
+    return composeOperators(this.operators, inputs);
   }
 }
