eaf905ae1d902a0c83a30ed5417a8abde374cde2
stone <stone-lyl@outlook.com>
2024-06-28 17:05:33 +0800

    feat: extend custom outputName of ElementPorts feature

--- packages/core/src/V2/nodes/linkCount.ts
+++ packages/core/src/V2/nodes/linkCount.ts
@@ -2,21 +2,25 @@
 // show the value pass numbe r of links
 
 import { CreateOutputPort, ElementPorts } from './elementPorts';
-import { map, tap } from 'rxjs/operators';
-import { OperatorElementConfig } from '../circuitElement';
+import { map } from 'rxjs/operators';
+import { OperatorElementConfig, PortProvider } from '../circuitElement';
 import { Operator } from './operator';
+import { Observable } from 'rxjs';
 
 export const linkCount: OperatorElementConfig = {
   boot: (param: unknown) => {
-    const linkCountParams = param as { getLinkCount: (count: number) => void };
+    const linkCountParams = param as {getLinkCount: (count: number) => void};
+
+    const getObservable = (input: PortProvider): Observable<unknown> => input.getPort('pipe').pipe(
+      map((val, i) => {
+        linkCountParams.getLinkCount(i + 1);
+        return val;
+      })
+    );
     const createLinkCountOutput: CreateOutputPort = (input) => {
-      return new ElementPorts(input.getPort('input').pipe(
-        map((val, i) => {
-          linkCountParams.getLinkCount(i + 1);
-          return val;
-        })
-      ))
+      return new ElementPorts(getObservable(input), 'pipe')
     }
+
     return new Operator(createLinkCountOutput, 'linkCount');
   }
 }
