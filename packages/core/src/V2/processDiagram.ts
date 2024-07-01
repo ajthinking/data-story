e205d4244db79d6a2bda1982c2d074f4451cab55
stone <stone-lyl@outlook.com>
2024-07-01 18:47:06 +0800

    feat: use inputPlaceholder replace outputPlaceholder

--- packages/core/src/V2/processDiagram.ts
+++ packages/core/src/V2/processDiagram.ts
@@ -1,36 +1,14 @@
-import { OperatorElement, PortProvider, SourceElement, WatcherElement, WatcherResult } from './circuitElement';
-import { Watcher } from './nodes/watcher';
-import { firstValueFrom, Observable } from 'rxjs';
+import {
+  DataStoryElement,
+  OperatorElement,
+  PortProvider,
+  SourceElement,
+  WatcherElement,
+  WatcherResult
+} from './circuitElement';
+import { asyncScheduler, firstValueFrom, mergeAll, Observable, scheduled } from 'rxjs';
 
-type SourceOperatorsWatcherArray = [SourceElement, ...OperatorElement[], WatcherElement];
-// [S, O, O..., W] => WatcherResult
-const validateDiagramArr = (diagramArr: SourceOperatorsWatcherArray): {
-  source: SourceElement,
-  operators: OperatorElement[],
-  watcher: WatcherElement
-} => {
-  if (diagramArr.length < 3) {
-    throw new Error('diagramArr should not less than 3 elements');
-  }
-
-  const source = diagramArr[0];
-  const operators = diagramArr.slice(1, -1) as OperatorElement[];
-  const watcher = diagramArr[diagramArr.length - 1];
-
-  // validate diagramArr, start with SourceElement, end with WatcherElement
-  if (source.elementType !== 'source' || watcher.elementType !== 'watcher') {
-    throw new Error('diagramArr should start with SourceElement and end with WatcherElement');
-  }
-
-  // validate operators, all elements should be OperatorElement
-  operators.forEach(operator => {
-    if (operator.elementType !== 'operator') {
-      throw new Error('diagramArr should only have OperatorElement between SourceElement and WatcherElement');
-    }
-  });
-
-  return { source, operators, watcher };
-};
+type SourceOperatorsWatcherArray = DataStoryElement[];
 
 export const composeOperators = (operators: OperatorElement[], input: PortProvider): PortProvider => {
   if (operators.length === 0) {
@@ -44,45 +22,23 @@ export const composeOperators = (operators: OperatorElement[], input: PortProvid
   }, operators[0].getOutput(input));
 }
 
-export const processDiagramArray = (diagramArr: SourceOperatorsWatcherArray): WatcherResult => {
-  const { source, operators, watcher } = validateDiagramArr(diagramArr);
-
-  return (watcher as Watcher).watch(
-    composeOperators(operators, source.getOutput())
-  );
-}
-// transform watcherResult to promise
-export const transformWatcherResultToPromise = async(diagram: WatcherResult) => {
-  return new Promise<void>(async(resolve, reject) => {
-    diagram.events.subscribe({
-      complete: () => {
-        console.log('completed');
-        resolve();
-      },
-      error: (e) => {
-        console.error(e, 'error');
-        reject(e);
-      }
-    });
-  });
-}
-
-class OutputPlaceholder implements PortProvider {
-  portProvider?: PortProvider;
+class InputPlaceholder implements PortProvider {
+  portProviders: PortProvider[] = [];
 
   getPort(portName: string) {
     // avoid portProvider is null when getPort is called
     return new Observable(subscriber => {
-      if (this.portProvider) {
-        this.portProvider.getPort(portName).subscribe(subscriber);
+      if (this.portProviders) {
+        const toBeMerged = this.portProviders.map(portProvider => portProvider.getPort(portName))
+        scheduled(toBeMerged, asyncScheduler).pipe(mergeAll()).subscribe(subscriber);
       } else {
         subscriber.complete();
       }
     });
   }
 
-  setPortProvider(portProvider: PortProvider) {
-    this.portProvider = portProvider;
+  addInput(portProvider: PortProvider) {
+    this.portProviders.push(portProvider);
   }
 }
 
@@ -91,35 +47,41 @@ class OutputPlaceholder implements PortProvider {
  */
 export class DiagramComposer {
   watchers = new Lookup<WatcherElement, () => WatcherResult>();
-  private outputMap = new Map<OperatorElement | SourceElement, PortProvider>();
+  private elementOutputs = new Map<OperatorElement | SourceElement, PortProvider>();
+  private inputPlaceholders = new Map<OperatorElement | WatcherElement, PortProvider>();
 
   addElements(elements: SourceOperatorsWatcherArray) {
     for(let element of elements) {
-      if (element.elementType === 'source' && !this.outputMap.has(element)) {
-        this.outputMap.set(element, element.getOutput());
+      if (element.elementType === 'source' && !this.elementOutputs.has(element)) {
+        this.elementOutputs.set(element, element.getOutput());
       }
-      if (element.elementType === 'operator' && !this.outputMap.has(element)) {
-        this.outputMap.set(element, new OutputPlaceholder());
+      if (element.elementType === 'operator' && !this.elementOutputs.has(element)) {
+        const inputPlaceholder: PortProvider = new InputPlaceholder();
+        this.inputPlaceholders.set(element, inputPlaceholder);
+        const output = element.getOutput(inputPlaceholder);
+        this.elementOutputs.set(element, output);
+      }
+      if (element.elementType === 'watcher' && !this.inputPlaceholders.has(element)) {
+        const watcherElement = element as WatcherElement;
+        let inputPlaceholder: PortProvider = new InputPlaceholder();
+        this.inputPlaceholders.set(watcherElement, inputPlaceholder);
+        this.watchers.add(watcherElement, () => watcherElement.watch(inputPlaceholder))
       }
     }
   }
 
-  addLink(link: OperatorElement, from: SourceElement | OperatorElement, to: OperatorElement | WatcherElement) {
-    const outputOfFrom = this.outputMap.get(from);
+  addLink(
+    link: OperatorElement,
+    from: SourceElement | OperatorElement,
+    to: OperatorElement | WatcherElement) {
+    const outputOfFrom = this.elementOutputs.get(from);
 
-    if(!outputOfFrom) {
+    if (!outputOfFrom) {
       throw new Error('from element is not found');
     }
 
-    if (to.elementType === 'watcher') {
-      this.watchers.add(to, () => to.watch(link.getOutput(outputOfFrom)));
-      return;
-    } else {
-      const opOutput = to.getOutput(link.getOutput(outputOfFrom));
-      const placeholder = this.outputMap.get(to) as OutputPlaceholder;
-      placeholder.setPortProvider(opOutput);
-      return;
-    }
+    const inputPlaceholder = this.inputPlaceholders.get(to) as InputPlaceholder;
+    inputPlaceholder.addInput(link.getOutput(outputOfFrom));
   }
 }
 
