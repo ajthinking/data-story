06aa6ee55419449d0e2e264b08c13d94715bf97c
stone <stone-lyl@outlook.com>
2024-07-01 18:58:49 +0800

    Test: add a test for multiple links in input or output port

--- packages/core/src/V2/examples/signal_sleep_consoleLog.ts
+++ packages/core/src/V2/examples/signal_sleep_consoleLog.ts
@@ -17,10 +17,31 @@ export const signal_sleep_consoleLog = async(params: SignalNodeParams) => {
   const consoleLogEle = consoleLog.boot();
 
   const diagramComposer = new DiagramComposer();
-  diagramComposer.addElements([signalEle, sleepEle, consoleLogEle]);
-  // diagramComposer.addLink(linkEle, sleepEle, consoleLogEle);
+  diagramComposer.addElements([signalEle, sleepEle,  consoleLogEle]);
+  diagramComposer.addLink(linkEle, sleepEle, consoleLogEle);
+  diagramComposer.addLink(linkEle, signalEle, sleepEle);
+
+  await runningTasks(diagramComposer);
+}
+
+export const signal_sleep_consoleLog_multiple = async(params: SignalNodeParams) => {
+  const signalEle = signal.boot(params);
+  const signalEle2 = signal.boot(params);
+  const linkCountEle = linkCount.boot({
+    getLinkCount: (count: number) => {
+      // console.log('linkCount:', count);
+    }
+  });
+  const linkEle = link.boot({ from: 'output', to: 'input', middleware: [linkCountEle] });
+  const sleepEle = sleep.boot({ period: 100 });
+  const consoleLogEle = consoleLog.boot();
+
+  const diagramComposer = new DiagramComposer();
+  diagramComposer.addElements([signalEle, sleepEle, signalEle2, consoleLogEle]);
+  diagramComposer.addLink(linkEle, sleepEle, consoleLogEle);
   diagramComposer.addLink(linkEle, signalEle, consoleLogEle);
   diagramComposer.addLink(linkEle, signalEle, sleepEle);
+  diagramComposer.addLink(linkEle, signalEle2, sleepEle);
 
   await runningTasks(diagramComposer);
 }
