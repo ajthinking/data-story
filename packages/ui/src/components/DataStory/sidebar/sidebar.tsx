1768e695d3c83fbcf2d5c84b0d08cb0eadf960a6
stone <stone-lyl@outlook.com>
2024-07-31 12:31:29 +0800

    feat: enable Sidebar component to access shared node state

--- packages/ui/src/components/DataStory/sidebar/sidebar.tsx
+++ packages/ui/src/components/DataStory/sidebar/sidebar.tsx
@@ -1,22 +1,32 @@
+import { NodeSettingsForm } from '../Form/nodeSettingsForm';
+import { ReactFlowNode } from '../../Node/ReactFlowNode';
+import { StoreSchema, useStore } from '../store/store';
+import { shallow } from 'zustand/shallow';
+
 export const Sidebar = () => {
-  // const selector = (state: StoreSchema) => ({
-  //   nodes: state.nodes,
-  //   openNodeModalId: state.openNodeModalId,
-  //   setOpenNodeModalId: state.setOpenNodeModalId,
-  //   updateNode: state.updateNode,
-  // });
-  // const { nodes, openNodeModalId, setOpenNodeModalId } = useStore(selector, shallow);
+  const selector = (state: StoreSchema) => ({
+    nodes: state.nodes,
+    openNodeModalId: state.openNodeModalId,
+    setOpenNodeModalId: state.setOpenNodeModalId,
+    updateNode: state.updateNode,
+  });
+  const { nodes, openNodeModalId, setOpenNodeModalId } = useStore(selector, shallow);
 
-  // const node = nodes.find((node: ReactFlowNode) => node.id === openNodeModalId)!
-  // const close = () => setOpenNodeModalId(null);
+  const node = nodes.find((node: ReactFlowNode) => node.id === openNodeModalId)!
+  const close = () => setOpenNodeModalId(null);
 
+  console.log(node, 'node')
   return (
     <div className="sidebar">
       <div className="sidebar-header">
         <h2>Data Stories</h2>
       </div>
       <div className="sidebar-content">
-        {/*<NodeSettingsForm node={node} close={close}/>*/}
+        {
+          Boolean(openNodeModalId)
+            ? <NodeSettingsForm node={node} close={close}/>
+            : <p> click one node </p>
+        }
       </div>
     </div>
   );
