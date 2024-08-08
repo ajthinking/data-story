04cecdd7c3690db5d52f0c2d450626f398b6e8bf
Anders Jürisoo <jurisoo@hotmail.com>
2024-08-08 12:15:59 +0200

    Restore tests

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,5 @@
 export { get } from './utils/get'
+export { sleep } from './utils/sleep'
 export { stringifyError } from './utils/stringifyError'
 export { serializeError } from './utils/serializeError'
 export { multiline } from './utils/multiline'
--- packages/ui/src/components/DataStory/clients/JsClient.test.ts
+++ packages/ui/src/components/DataStory/clients/JsClient.test.ts
@@ -1,102 +1,63 @@
-import { describe, it, expect, beforeEach, vi, afterEach } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { JsClient } from './JsClient';
-import { Application, Diagram, Executor, NodeDescription, Registry } from '@data-story/core';
-
-vi.mock('@data-story/core', () => {
-  const originalModule = vi.importActual('@data-story/core');
-
-  const executor = vi.fn(() => ({
-    execute: vi.fn(),
-  }))
-
-  return {
-    ...originalModule,
-    ExecutorFactory: {
-      create: executor,
-    },
-    Executor: executor,
-    Diagram: vi.fn(),
-    InMemoryStorage: vi.fn(),
-    InputObserverController: vi.fn(),
-  };
-});
+import { Application, coreNodeProvider, Diagram, sleep } from '@data-story/core';
 
 describe('JsClient', () => {
-  let setAvailableNodesMock: (nodes: NodeDescription[]) => void;
-  let updateEdgeCountsMock: (edgeCounts: Record<string, number>) => void;
-  let appMock: Application;
-  let client: JsClient;
-  let consoleLogSpy: ReturnType<typeof vi.spyOn>;
-
-  beforeEach(() => {
-    setAvailableNodesMock = vi.fn();
-    updateEdgeCountsMock = vi.fn();
-    appMock = {
-      descriptions: vi.fn().mockReturnValue([]),
-      computers: [],
-      hooks: new Map(),
-      getRegistry: () => {
-        return {};
-      }
-      // Add any other properties or methods expected by the Application type
-    } as unknown as Application; // Cast to Application if necessary
-    client = new JsClient({
-      setAvailableNodes: setAvailableNodesMock,
-      updateEdgeCounts: updateEdgeCountsMock,
-      app: appMock
-    });
-    consoleLogSpy = vi.spyOn(console, 'log').mockImplementation(() => {}) as any;
-  });
-
-  afterEach(() => {
-    consoleLogSpy.mockRestore();
-  });
-
   it('should set available nodes and log connection on init', () => {
+    const setAvailableNodesMock = vi.fn();
+    let consoleLogSpy = vi.spyOn(console, 'log')
+      .mockImplementation(() => {}) as any;
+
+    const client = new JsClient({
+      setAvailableNodes: setAvailableNodesMock,
+      updateEdgeCounts: vi.fn(),
+      app: new Application(),
+    })
+
     client.init();
     expect(setAvailableNodesMock).toHaveBeenCalledWith([]);
-    expect(console.log).toHaveBeenCalledWith('Connected to server: JS');
+    expect(consoleLogSpy).toHaveBeenCalledWith('Connected to server: JS');
   });
 
   it('should execute diagram and handle updates on run', async() => {
-    const diagramMock = new Diagram();
+    const setAvailableNodesMock = vi.fn();
+    const updateEdgeCountsMock = vi.fn();
+    let consoleLogSpy = vi.spyOn(console, 'log')
+      .mockImplementation(() => {}) as any;
 
-    // Mock the Executor class with the required arguments
-    const executorMock = {
-      execute: vi.fn(() => {
-        let hasYielded = false; // Closure variable to track the state
-        return {
-          [Symbol.asyncIterator]: () => ({
-            next: () => {
-              if (!hasYielded) {
-                hasYielded = true; // Update the state to indicate the value has been yielded
-                return Promise.resolve({
-                  value: { counts: { '1': 1 }, hooks: [{ type: 'CONSOLE_LOG', args: ['Log message'] }] },
-                  done: false
-                });
-              } else {
-                return Promise.resolve({ value: undefined, done: true });
-              }
-            }
-          }),
-        };
-      }),
-    };
+    const app = new Application();
+    app.register(coreNodeProvider);
+    app.boot();
 
-    // Replace the Executor's constructor with a mock that returns the executorMock
-    vi.mocked(Executor).mockImplementation(() => executorMock as unknown as Executor);
+    const client = new JsClient({
+      setAvailableNodes: setAvailableNodesMock,
+      updateEdgeCounts: updateEdgeCountsMock,
+      app,
+    });
 
-    await client.run(diagramMock);
+    const diagram = app.getDiagramBuilderV3()
+      .add('Create')
+      .add('ConsoleLog')
+      .connect()
+      .get();
 
-    expect(executorMock.execute).toHaveBeenCalled();
-    expect(updateEdgeCountsMock).toHaveBeenCalledWith({ '1': 1 });
-    expect(console.log).toHaveBeenCalledWith('Log message');
+    client.run(diagram);
+    await sleep(1) // Allow time for the executor to execute
 
-    await client.run(diagramMock);
-    expect(console.log).toHaveBeenCalledWith('Execution complete 💫');
+    expect(updateEdgeCountsMock).toHaveBeenCalledWith(
+      { 'Create.1--->ConsoleLog.1': 1},
+    );
+
+    expect(consoleLogSpy).toHaveBeenCalledWith('Execution complete 💫');
   });
 
   it('should not throw when save is called', async() => {
+    const client = new JsClient({
+      setAvailableNodes: vi.fn(),
+      updateEdgeCounts: vi.fn(),
+      app: new Application(),
+    });
+
     const diagram = new Diagram();
 
     await expect(client.save('test', diagram)).resolves.not.toThrow();
