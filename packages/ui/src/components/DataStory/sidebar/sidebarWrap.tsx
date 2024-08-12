9ecbf5e91cc043d9c52b0b9145b1d3c02d4b2b9a
stone <stone-lyl@outlook.com>
2024-08-12 17:02:34 +0800

    feat: add SidebarWrap component

---
+++ packages/ui/src/components/DataStory/sidebar/sidebarWrap.tsx
@@ -0,0 +1,72 @@
+import { ReactNode, useEffect, useRef } from 'react';
+import { useEscapeKey } from '../hooks/useEscapeKey';
+import { CollapseIcon } from '../icons/collapseIcon';
+
+export function SidebarWrap({
+  setShowSidebar,
+  title,
+  children,
+  primaryAction,
+  onPrimaryAction,
+}: {
+  setShowSidebar: (sidebar: string) => void;
+  title?: string;
+  children?: ReactNode;
+  primaryAction?: string;
+  onPrimaryAction?: () => void;
+}) {
+  useEscapeKey(() => setShowSidebar(''));
+
+  const sidebarRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target as Node)) {
+        setShowSidebar('');
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [sidebarRef]);
+
+  return <>
+    <div ref={sidebarRef} className="relative flex flex-col w-full h-full">
+      {title && (
+        <div className="px-6 py-4 border-b border-solid border-slate-200 flex flex-row justify-between">
+          <div className="text-lg font-bold text-gray-400">
+            {title}
+          </div>
+          <button
+            className="p-1 ml-auto text-black text-3xl leading-none font-semibold outline-none focus:outline-none"
+            onClick={() => setShowSidebar('')}>
+            <span className="text-gray-500 h-6 w-6 block outline-none focus:outline-none" title={'sss'}>
+              <CollapseIcon/>
+            </span>
+          </button>
+        </div>
+      )}
+      <div className="flex-auto overflow-y-auto">
+        {children}
+      </div>
+      {primaryAction && (
+        <div className="px-6 py-4 border-t border-solid border-slate-200">
+          <button
+            className="text-gray-500 background-transparent font-bold uppercase text-sm px-4 py-2 outline-none focus:outline-none mr-2 ease-linear transition-all duration-150"
+            type="button" onClick={() => setShowSidebar('')}>
+            Close
+          </button>
+          {primaryAction && (
+            <button
+              className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150"
+              type="button" onClick={onPrimaryAction}>
+              {primaryAction}
+            </button>
+          )}
+        </div>
+      )}
+    </div>
+  </>;
+}
