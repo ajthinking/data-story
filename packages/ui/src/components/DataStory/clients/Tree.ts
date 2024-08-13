a2bc22f4330c6b418cea56e0c4e2ff1fb7b27f8d
Anders Jürisoo <jurisoo@hotmail.com>
2024-08-13 10:12:55 +0200

    Extract Tree

---
+++ packages/ui/src/components/DataStory/clients/Tree.ts
@@ -0,0 +1,8 @@
+import { Diagram } from '@data-story/core';
+
+export interface Tree<TreeNodeType = Diagram> {
+  path: string;
+  type: 'file' | 'folder';
+  content?: TreeNodeType;
+  children?: Tree[];
+}
