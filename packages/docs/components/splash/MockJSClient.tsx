02d9769a123c76c717535dce5ce6e94dd4000066
stone <stone-lyl@outlook.com>
2024-08-19 16:10:58 +0800

    feat: add mock JSClient class

---
+++ packages/docs/components/splash/MockJSClient.tsx
@@ -0,0 +1,23 @@
+import { Diagram, NodeDescription } from '@data-story/core';
+import { WorkspacesApi } from '@data-story/ui';
+
+export class JSClient {
+  private digram: Diagram;
+
+  constructor(digram: Diagram) {
+    this.digram = digram;
+  }
+
+  workspacesApi: WorkspacesApi = {
+    getNodeDescriptions: async({ path }) => {
+      return [] as NodeDescription[]
+    },
+    getTree: async({ path }) => {
+      return Promise.resolve({
+        path: '/',
+        type: 'file',
+        content: this.digram,
+      });
+    }
+  } as WorkspacesApi;
+}
