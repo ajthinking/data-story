bf8479ce4c72068bdb91d09d6a506ab65f20ae03
Anders Jürisoo <jurisoo@hotmail.com>
2024-08-20 09:23:25 +0200

    Implement TreeManager methods

--- packages/core/src/TreeManager.ts
+++ packages/core/src/TreeManager.ts
@@ -1,3 +1,33 @@
-export class TreeManager {
+import { Tree } from './Tree';
 
+export interface TreeManager {
+  getTree(): Promise<Tree>;
+  createTree: ({ path, tree }: { path: string, tree: Tree }) => Promise<Tree>;
+  updateTree: ({ path, tree }: { path: string, tree: Tree }) => Promise<Tree>
+  destroyTree: ({ path }: { path: string }) => Promise<void>
+  moveTree: ({ path, newPath }: { path: string, newPath: string}) => Promise<Tree>
+}
+
+export class NullTreeManager implements TreeManager {
+  async getTree(): Promise<Tree> {
+    return {
+      path: '/',
+      type: 'folder',
+    }
+  }
+  async createTree({ path, tree }: { path: string, tree: Tree }): Promise<Tree> {
+    return tree;
+  }
+  async updateTree({ path, tree }: { path: string, tree: Tree }): Promise<Tree> {
+    return tree;
+  }
+  async destroyTree({ path }: { path: string }): Promise<void> {
+    return;
+  }
+  async moveTree({ path, newPath }: { path: string, newPath: string}): Promise<Tree> {
+    return {
+      path: newPath,
+      type: 'folder',
+    }
+  }
 }
