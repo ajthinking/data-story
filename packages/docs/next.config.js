e91bd296c3c93a5cb96486bb811d41465f83f038
stone <stone-lyl@outlook.com>
2024-09-06 11:49:20 +0800

    feat: add script analyze docs size

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -1,3 +1,5 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
@@ -22,6 +24,14 @@ module.exports = {
         use: ['source-map-loader'],
       });
       baseConfig.ignoreWarnings = [/Failed to parse source map/];
+    }
+    if (!context.isServer) {
+      config.plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        analyzerPort: context.isServer ? 8888 : 8889,
+        reportFilename: './analyze/server.html',
+        openAnalyzer: process.env.ANALYZER === 'true',
+      }));
     }
 
     baseConfig.devServer = {
