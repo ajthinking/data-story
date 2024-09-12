58321a32eeb1e9685072e46e7f2b74652070313a
stone <stone-lyl@outlook.com>
2024-09-12 17:51:54 +0800

    fix: resolve `ComputerConfigCreate` not found issue in `DiagramBuilderV3.add`

--- packages/docs/components/demos/TinkerDemo.tsx
+++ packages/docs/components/demos/TinkerDemo.tsx
@@ -2,24 +2,31 @@ import { DataStory } from '@data-story/ui'
 import { core, str, } from '@data-story/core';
 import { MockJSClient } from '../splash/MockJSClient';
 import { useRequestApp } from '../hooks/useRequestApp';
+import { useMemo } from 'react';
+import useRequest from 'ahooks/lib/useRequest';
 
 // This component is just a place to sketch
 export default () => {
-  const { app, loading } = useRequestApp();
+  const { app, loading: appLoading } = useRequestApp();
 
-  const diagram = core.getDiagramBuilderV3()
-    .add('Create')
-    .get()
+  const { data: diagram, loading: diagramLoading } = useRequest(async() => {
+    await core.boot();
+    const diagram = core.getDiagramBuilderV3()
+      .add('Create')
+      .get()
 
-  diagram.params = [
-    str({
-      name: 'message',
-      help: 'A message to pass on into the execution.',
-    })
-  ]
-  const client = new MockJSClient({ diagram: diagram, app });
+    diagram.params = [
+      str({
+        name: 'message',
+        help: 'A message to pass on into the execution.',
+      })
+    ]
+    return diagram;
+  });
 
-  if (loading || !client) return null;
+  const client = new MockJSClient({ diagram, app });
+
+  if (appLoading || !client || diagramLoading) return null;
   return (
     <div className="w-full h-1/2">
       <DataStory
