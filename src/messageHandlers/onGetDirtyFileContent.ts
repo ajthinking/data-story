d259bcfc30aeef59e9c60764304956404df6b444
Anders Jürisoo <jurisoo@hotmail.com>
2024-09-22 12:38:38 +0200

    Better decode

--- src/messageHandlers/onGetDirtyFileContent.ts
+++ src/messageHandlers/onGetDirtyFileContent.ts
@@ -3,6 +3,6 @@ import { MessageHandler } from '../MessageHandler';
 export const onGetDirtyFileContent: MessageHandler = async ({ event, webviewPanel, document }) => {
   webviewPanel.webview.postMessage({
     type: 'dirtyFileContent',
-    fileContent: document.data.toString()
+    fileContent: new TextDecoder().decode(document.data),
   });
 }
