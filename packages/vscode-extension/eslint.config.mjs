62d703bc804c7b70877521cec8eeacefc6f4b4f3
Anders Jürisoo <jurisoo@hotmail.com>
2024-09-28 09:08:13 +0200

    Hamronize vscode-extension code style

--- packages/vscode-extension/eslint.config.mjs
+++ packages/vscode-extension/eslint.config.mjs
@@ -1,31 +1,52 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import stylistic from '@stylistic/eslint-plugin';
+import reactHooks from 'eslint-plugin-react-hooks';
+import importPlugin from 'eslint-plugin-import';
 
 export default [{
-    files: ["**/*.ts"],
+  files: ['**/*.ts'],
 }, {
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-    },
+  plugins: {
+    '@typescript-eslint': typescriptEslint,
+    '@stylistic': stylistic,
+    'react-hooks': reactHooks,
+    'import': importPlugin,
+  },
+  languageOptions: {
+    parser: tsParser,
+    ecmaVersion: 2022,
+    sourceType: 'module',
     globals: {
-      acquireVsCodeApi: "readonly"  // Add this line to define acquireVsCodeApi as a global variable
-    },    
-
-    languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+      acquireVsCodeApi: 'readonly'  // Move globals here
     },
+  },
 
-    rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
+  rules: {
+    'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 0, 'maxBOF': 0 }],
+    'padded-blocks': ['error', { 'blocks': 'never' }],
+    'no-trailing-spaces': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@stylistic/indent': [
+      'error',
+      2
+    ],
+    '@stylistic/linebreak-style': 'off',
+    '@stylistic/quotes': [
+      'error',
+      'single'
+    ],
+    '@stylistic/eol-last': 'off',
+    '@stylistic/space-infix-ops': ['warn', { 'int32Hint': false }],
+    '@typescript-eslint/naming-convention': ['warn', {
+      selector: 'import',
+      format: ['camelCase', 'PascalCase'],
+    }],
 
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
-    },
+    curly: 'warn',
+    eqeqeq: 'warn',
+    'no-throw-literal': 'warn',
+    semi: 'warn',
+  },
 }];
