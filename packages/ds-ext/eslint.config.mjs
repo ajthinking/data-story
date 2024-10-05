be3d9219ab24c7c63d5be188ea515e6f18dfacf4
stone <stone-lyl@outlook.com>
2024-10-05 16:16:07 +0800

    feat: update ESLint configuration

--- packages/ds-ext/eslint.config.mjs
+++ packages/ds-ext/eslint.config.mjs
@@ -4,9 +4,8 @@ import stylistic from '@stylistic/eslint-plugin';
 import reactHooks from 'eslint-plugin-react-hooks';
 import importPlugin from 'eslint-plugin-import';
 
-export default [{
-  files: ['**/*.ts'],
-}, {
+export default {
+  files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
   plugins: {
     '@typescript-eslint': typescriptEslint,
     '@stylistic': stylistic,
@@ -21,10 +20,9 @@ export default [{
       acquireVsCodeApi: 'readonly'  // Move globals here
     },
   },
-
   rules: {
-    'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 0, 'maxBOF': 0 }],
-    'padded-blocks': ['error', { 'blocks': 'never' }],
+    'no-multiple-empty-lines': [ 'error', { 'max': 1, 'maxEOF': 0, 'maxBOF': 0 } ],
+    'padded-blocks': [ 'error', { 'blocks': 'never' } ],
     'no-trailing-spaces': 'error',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
@@ -38,15 +36,14 @@ export default [{
       'single'
     ],
     '@stylistic/eol-last': 'off',
-    '@stylistic/space-infix-ops': ['warn', { 'int32Hint': false }],
-    '@typescript-eslint/naming-convention': ['warn', {
+    '@stylistic/space-infix-ops': [ 'error', { 'int32Hint': false }],
+    '@typescript-eslint/naming-convention': [ 'warn', {
       selector: 'import',
-      format: ['camelCase', 'PascalCase'],
-    }],
-
+      format: [ 'camelCase', 'PascalCase' ],
+    } ],
     curly: 'warn',
     eqeqeq: 'warn',
     'no-throw-literal': 'warn',
     semi: 'warn',
   },
-}];
+};
