a310028fdb08828f64b0ff9a93780fd138fa676e
stone <stone-lyl@outlook.com>
2024-11-28 10:57:39 +0800

    feat: implement `useObserverConsole` to observe console node items.

--- packages/ui/src/components/Node/ConsoleNodeComponent.tsx
+++ packages/ui/src/components/Node/ConsoleNodeComponent.tsx
@@ -1,14 +1,47 @@
 import React, { memo } from 'react';
 import { DataStoryNodeData } from './ReactFlowNode';
 import { NodeComponent } from '../../index';
+import { useStore } from '../DataStory/store/store';
+import { shallow } from 'zustand/shallow';
+import { Subscription } from 'rxjs';
+import { useMount, useUnmount } from 'ahooks';
+import { ItemsObserver, RequestObserverType } from '@data-story/core';
+import { StoreSchema } from '../DataStory/types';
 
 const ConsoleNodeComponent = ({ id, data, selected }: {
   id: string,
   data: DataStoryNodeData
   selected: boolean
 }) => {
-  console.log('ConsoleNodeComponent', id, data, selected)
+  useObserverConsole({ id });
   return <NodeComponent id={id} data={data} selected={selected}/>
 }
 
+const useObserverConsole = ({ id }: {id: string}) => {
+  const selector = (state: StoreSchema) => ({
+    toDiagram: state.toDiagram,
+    client: state.client,
+  });
+  const { toDiagram, client } = useStore(selector, shallow);
+
+  let consoleSubscription: Subscription;
+  // Add the node to the inputObservers when the node is mounted
+  useMount(() => {
+    const linkId = toDiagram()?.getLinkIdFromNodeId?.(id, 'input');
+    if (!client?.itemsObserver || !linkId) return;
+    const consoleObserver: ItemsObserver = {
+      linkIds: [linkId],
+      type: RequestObserverType.itemsObserver,
+      onReceive: (batchedItems) => {
+        console.log(...batchedItems ?? []);
+      }
+    }
+
+    consoleSubscription = client?.itemsObserver?.(consoleObserver);
+  });
+
+  useUnmount(() => {
+    consoleSubscription?.unsubscribe();
+  });
+}
 export default memo(ConsoleNodeComponent);
