4703b8bf582504ee13ae47f31ea72e1d1f28ad23
stone <stone-lyl@outlook.com>
2024-12-17 16:11:02 +0800

    feat: Replace bind method with arrow functions and resolve demo errors.

--- packages/docs/components/splash/CustomizeJSClient.tsx
+++ packages/docs/components/splash/CustomizeJSClient.tsx
@@ -1,4 +1,13 @@
-import { Application, Diagram, NodeDescription,ObserveLinkItems,ObservelinkCounts } from '@data-story/core';
+import {
+  Application,
+  Diagram,
+  NodeDescription,
+  ObserveLinkItems,
+  ObservelinkCounts,
+  ObserveLinkUpdate,
+  GetDataFromStorage,
+  ObserveNodeStatus
+} from '@data-story/core';
 import { ClientRunParams, createJSClient, WorkspaceApiClient, WorkspaceApiClientImplement } from '@data-story/ui';
 
 export class CustomizeJSClient implements WorkspaceApiClientImplement {
@@ -36,6 +45,18 @@ export class CustomizeJSClient implements WorkspaceApiClientImplement {
 
   observeLinkCounts = (params: ObservelinkCounts) => {
     return this.jsClient.observeLinkCounts(params);
+  }
+
+  observeNodeStatus = (params: ObserveNodeStatus) => {
+    return this.jsClient.observeNodeStatus(params);
+  }
+
+  observeLinkUpdate = (params: ObserveLinkUpdate) => {
+    return this.jsClient.observeLinkUpdate(params);
+  }
+
+  getDataFromStorage = (params: GetDataFromStorage) => {
+    return this.jsClient.getDataFromStorage(params);
   }
 
   cancelObservation = (params) => {
