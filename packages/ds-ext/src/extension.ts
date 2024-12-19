74b5e5693847d755a158366192f4a5d828b01032
stone <stone-lyl@outlook.com>
2024-12-19 22:14:30 +0800

    feat: Add `yarn install` command support

--- packages/ds-ext/src/extension.ts
+++ packages/ds-ext/src/extension.ts
@@ -3,6 +3,7 @@ import { DiagramEditorProvider } from './DiagramEditorProvider';
 import * as fs from 'fs';
 import * as path from 'path';
 import { createDemosDirectory } from './commands/createDemosDirectory';
+import { spawn } from 'child_process';
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand('ds-ext.createDemos', async () => {
@@ -15,6 +16,20 @@ export function activate(context: vscode.ExtensionContext) {
       new DiagramEditorProvider(context)
     )
   );
+
+  console.log('ds-ext is installed at ', context.extensionPath);
+  // run yarn install in the extension folder
+  const extensionPath = context.extensionPath;
+  // hide cmd window
+  const yarnInstall = spawn('yarn', ['install'], { cwd: extensionPath, stdio: 'inherit'
+    , shell: false
+  });
+  yarnInstall.on('close', (code) => {
+    console.log(`yarn install exited with code ${code}`);
+  });
+  yarnInstall.on('error', (err) => {
+    console.log('yarn install failed', err);
+  });
 }
 
 export function deactivate() {}
