dd5f45488470fd7db0ee6928a2cd3bc746f135c0
stone <stone-lyl@outlook.com>
2024-12-20 16:43:41 +0800

    feat: Set database-dependent operations to be asynchronous.

--- packages/ds-ext/src/messageHandlers/getDataFromStorage.ts
+++ packages/ds-ext/src/messageHandlers/getDataFromStorage.ts
@@ -6,7 +6,7 @@ import {
 } from '@data-story/core';
 
 export const getDataFromStorage: MessageHandler = async({ event, webviewPanel, inputObserverController }) => {
-  const result: Record<LinkId, ItemValue[]> = inputObserverController.getDataFromStorage(event as GetDataFromStorage);
+  const result: Record<LinkId, ItemValue[]> = await inputObserverController.getDataFromStorage(event as GetDataFromStorage);
   webviewPanel.webview.postMessage({
     ...event,
     data: result,
