c536677547e024bea0af7d9454112c5c2ca33e92
stone <stone-lyl@outlook.com>
2024-12-24 23:49:58 +0800

    feat: Add ds-ext output channel and automate installation of duckdb-async in the designated path.

--- packages/ds-ext/src/extension.ts
+++ packages/ds-ext/src/extension.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode';
 import { DiagramEditorProvider } from './DiagramEditorProvider';
 import { createDemosDirectory } from './commands/createDemosDirectory';
-import { exec, spawn } from 'child_process';
 
 let diagramEditorProvider: DiagramEditorProvider;
 
@@ -9,34 +8,37 @@ export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand('ds-ext.createDemos', async () => {
     createDemosDirectory();
   });
-
-  console.log('Congratulations, your extension "ds-ext" is now active!', context);
-  console.log('ds-ext is installed at ', context.extensionPath);
-  // run yarn install in the extension folder
-  const extensionPath = context.extensionPath;
-  // 提示用户是否安装 duckdb-async
-  vscode.window.showInformationMessage(
-    'Do you want to install duckdb-async?',
-    'Yes',
-    'No'
-  ).then(selection => {
-    if (selection === 'Yes') {
-      // 创建一个新的终端
-      const terminal = vscode.window.createTerminal('DuckDB Setup', extensionPath);
-      // 在终端中运行安装命令
-      terminal.show();
-      terminal.sendText('npm install duckdb-async');
-    }
-  });
+  const outputChannel = vscode.window.createOutputChannel('DS-Ext');
+  outputChannel.appendLine('Congratulations, your extension "ds-ext" is now active!');
 
   diagramEditorProvider = new DiagramEditorProvider(context);
-
   context.subscriptions.push(
     vscode.window.registerCustomEditorProvider(
       'ds-ext.diagramEditor',
       diagramEditorProvider
     )
   );
+
+  const installScriptsPath = `${context.extensionPath}/install-scripts`;
+
+  outputChannel.appendLine(`ds-ext is installed at ${context.extensionPath}`);
+  outputChannel.show();
+
+  vscode.window.showInformationMessage(
+    'The "ds-ext" extension requires "duckdb-async". Would you like to install it now?',
+    'Yes',
+    'No'
+  ).then(selection => {
+    if (selection === 'Yes') {
+      // Create a new terminal
+      const terminal = vscode.window.createTerminal('DuckDB Setup');
+      terminal.show();
+      // Change directory to the extension path
+      terminal.sendText(`cd "${installScriptsPath}"`);
+      // run the install command
+      terminal.sendText('npm install duckdb-async');
+    }
+  });
 }
 
 export function deactivate(context: any) {
