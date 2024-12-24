2a73f2a3c7ca237b1e7bea48751948bb19d3214b
stone <stone-lyl@outlook.com>
2024-12-24 23:00:07 +0800

    feat: Implement automatic installation command for the duckdb-async extension.

--- packages/ds-ext/src/extension.ts
+++ packages/ds-ext/src/extension.ts
@@ -1,15 +1,32 @@
 import * as vscode from 'vscode';
 import { DiagramEditorProvider } from './DiagramEditorProvider';
-import * as fs from 'fs';
-import * as path from 'path';
 import { createDemosDirectory } from './commands/createDemosDirectory';
-import { spawn } from 'child_process';
+import { exec, spawn } from 'child_process';
 
 let diagramEditorProvider: DiagramEditorProvider;
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand('ds-ext.createDemos', async () => {
     createDemosDirectory();
+  });
+
+  console.log('Congratulations, your extension "ds-ext" is now active!', context);
+  console.log('ds-ext is installed at ', context.extensionPath);
+  // run yarn install in the extension folder
+  const extensionPath = context.extensionPath;
+  // 提示用户是否安装 duckdb-async
+  vscode.window.showInformationMessage(
+    'Do you want to install duckdb-async?',
+    'Yes',
+    'No'
+  ).then(selection => {
+    if (selection === 'Yes') {
+      // 创建一个新的终端
+      const terminal = vscode.window.createTerminal('DuckDB Setup', extensionPath);
+      // 在终端中运行安装命令
+      terminal.show();
+      terminal.sendText('npm install duckdb-async');
+    }
   });
 
   diagramEditorProvider = new DiagramEditorProvider(context);
@@ -20,20 +37,6 @@ export function activate(context: vscode.ExtensionContext) {
       diagramEditorProvider
     )
   );
-
-  console.log('ds-ext is installed at ', context.extensionPath);
-  // run yarn install in the extension folder
-  const extensionPath = context.extensionPath;
-  // hide cmd window
-  const yarnInstall = spawn('yarn', ['install'], { cwd: extensionPath, stdio: 'inherit'
-    , shell: false
-  });
-  yarnInstall.on('close', (code) => {
-    console.log(`yarn install exited with code ${code}`);
-  });
-  yarnInstall.on('error', (err) => {
-    console.log('yarn install failed', err);
-  });
 }
 
 export function deactivate(context: any) {
