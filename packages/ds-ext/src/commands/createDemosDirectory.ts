57fa773535757640eee1170a6e3d15c9c927cb39
Anders Jürisoo <jurisoo@hotmail.com>
2025-01-03 11:21:04 +0100

    Make Density demo datasets

--- packages/ds-ext/src/commands/createDemosDirectory.ts
+++ packages/ds-ext/src/commands/createDemosDirectory.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as demos from './demos';
+import { makeDensityDatasets } from './makeDensityDatasets';
 
 export async function createDemosDirectory() {
   const workspaceFolders = vscode.workspace.workspaceFolders;
@@ -31,6 +32,9 @@ export async function createDemosDirectory() {
       { id: 2, title: 'Build a DataStory extension', completed: false },
       { id: 3, title: 'Profit', completed: false },
     ], null, 2));
+
+    // Create density datasets
+    makeDensityDatasets(path.join(demosDir, 'data', 'densities'));
 
     for (const [moduleName, demoFactory] of Object.entries(demos)) {
       const filePath = path.join(demosDir, `${moduleName}.diagram.json`);
