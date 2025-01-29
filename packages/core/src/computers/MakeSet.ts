418fb2bbb99ddb67ed70797bdb8095c6fdb2b286
Anders Jürisoo <jurisoo@hotmail.com>
2025-01-29 09:24:28 +0100

    Add MakeSet

---
+++ packages/core/src/computers/MakeSet.ts
@@ -0,0 +1,44 @@
+import { str } from '../Param';
+import { Computer } from '../types/Computer';
+
+export const MakeSet: Computer = {
+  name: 'MakeSet',
+  label: 'MakeSet',
+  inputs: [{
+    name: 'input',
+    schema: {},
+  }],
+  outputs: [{
+    name: 'output',
+    schema: {},
+  }],
+  params: [
+    str({
+      name: 'property',
+      label: 'Property',
+      value: '',
+      help: 'The property to create unique items from',
+      multiline: false,
+      canInterpolate: false,
+    }),
+  ],
+  async *run({ input, output, params }) {
+    const seen = new Set();
+
+    while (true) {
+      const items = input.pull();
+
+      for (const item of items) {
+        const property = params.property as string;
+        const value = item.value[property];
+
+        if (!seen.has(value)) {
+          seen.add(value);
+          output.push([{ [property]: value }]);
+        }
+      }
+
+      yield;
+    }
+  },
+};
