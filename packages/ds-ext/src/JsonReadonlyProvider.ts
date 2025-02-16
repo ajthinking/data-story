f2932ba70ebe32638ddfd618fbea898343a8289d
stone <stone-lyl@outlook.com>
2025-02-16 18:20:27 +0800

    feat: remove the Chinese characters

--- packages/ds-ext/src/JsonReadonlyProvider.ts
+++ packages/ds-ext/src/JsonReadonlyProvider.ts
@@ -1,15 +1,16 @@
 import vscode from 'vscode';
 
 export class JsonReadonlyProvider implements vscode.TextDocumentContentProvider {
-  // 存储各 URI 对应的内容
+  // Store the content corresponding to each URI
   private contentMap = new Map<string, string>();
   readonly onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
   readonly onDidChange = this.onDidChangeEmitter.event;
 
-  // 更新指定 URI 的内容
+  // Update the content of the specified URI
   updateContent(uri: vscode.Uri, content: string) {
     this.contentMap.set(uri.toString(), content);
-    this.onDidChangeEmitter.fire(uri); // 通知 VS Code 内容更新
+    // Notify VS Code that the content has been updated
+    this.onDidChangeEmitter.fire(uri);
   }
 
   provideTextDocumentContent(uri: vscode.Uri): string {
