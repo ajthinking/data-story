7e0d5e1b2aa0162fe75017ea8a4cad8cb1ba6eff
stone <stone-lyl@outlook.com>
2025-03-14 15:15:57 +0800

    test: Fix unsubscribe mock in ConsoleNodeComponent test

--- packages/ui/src/components/Node/ConsoleNodeComponent.cy.tsx
+++ packages/ui/src/components/Node/ConsoleNodeComponent.cy.tsx
@@ -2,6 +2,7 @@ import ConsoleNodeComponent from './ConsoleNodeComponent';
 import { ReactFlowProvider } from '@xyflow/react';
 import { LinkItemsParam, ObserveLinkItems } from '@data-story/core';
 import { DataStoryContext } from '../DataStory/store/store';
+import { WorkspaceApiClientImplement } from '../DataStory/clients/WorkspaceApiClientImplement';
 
 const data = {
   'params': [],
@@ -18,7 +19,7 @@ const data = {
 };
 const id = 'ConsoleLog.1';
 
-const mountConsoleNodeComponent = (items: unknown[], client?: () => void) => {
+const mountConsoleNodeComponent = (items: unknown[], client?: () => WorkspaceApiClientImplement) => {
   cy.mount(
     // @ts-ignore
     <DataStoryContext.Provider value={() => {
@@ -31,7 +32,7 @@ const mountConsoleNodeComponent = (items: unknown[], client?: () => void) => {
         client: client?.() || {
           observeLinkItems: (observer: ObserveLinkItems) => {
             observer.onReceive(items as LinkItemsParam[]);
-            return { unsubscribe: () => { const spy = cy.spy(); return spy; } };
+            return { subscribe:() => cy.spy() };
           },
         },
       })
