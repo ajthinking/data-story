3e50cf4faacfc29040d2d7c535e3c5d685a2ac0a
stone <stone-lyl@outlook.com>
2025-03-31 17:29:48 +0800

    build: Add postcss and webpack config for ui package

---
+++ packages/ui/postcss.config.js
@@ -0,0 +1,8 @@
+/** @type {import('postcss-load-config').Config} */
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ packages/ui/webpack.config.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const dependencies = require('./package.json').dependencies;
+
+console.log('dependencies', dependencies);
+const externalsDeps = Object.keys(dependencies).filter((dep) => {
+  return !dep.includes('codemirror');
+});
+// const externalsDeps = dependencies;
+console.log('externalsDeps', externalsDeps);
+const commonJSConfig = (env, options) => ({
+  devtool: 'source-map',
+  mode: 'development',
+  entry: './src/index.ts',
+
+  output: {
+    // in browse how to get path path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.cjs',
+    libraryTarget: 'commonjs2',
+    clean: !options.watch,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/',
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader', 'postcss-loader' ],
+      },
+    ],
+  },
+  externals: [
+    'react',
+    'react-dom',
+    'react/jsx-runtime',
+    ...Object.keys(externalsDeps),
+  ],
+  resolve: {
+    extensions: [ '.js', '.jsx', '.ts', '.tsx' ],
+  },
+});
+
+const esmConfig = (env, options) => ({
+  devtool: 'source-map',
+  entry: './src/index.ts',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.mjs',
+    libraryTarget: 'module',
+    clean: !options.watch,
+    library: {
+      type: 'module',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/',
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader', 'postcss-loader' ],
+      },
+    ],
+  },
+  externals: [
+    'react',
+    'react-dom',
+    'react/jsx-runtime',
+    ...Object.keys(externalsDeps),
+  ],
+  resolve: {
+    extensions: [ '.js', '.jsx', '.ts', '.tsx' ],
+  },
+  externalsType: 'module',
+  experiments: {
+    outputModule: true,
+  },
+});
+
+module.exports = function () {
+  return [ commonJSConfig(...arguments), esmConfig(...arguments) ];
+};
+
+// export const configs = { commonJSConfig, esmConfig };
