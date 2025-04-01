6132fc21806d702de1f963bdbbae5a5fe616b1cc
stone <stone-lyl@outlook.com>
2025-04-01 20:30:59 +0800

    feat: configure tsconfig in Cypress Webpack for consistency with UI project

--- packages/ui/cypress-webpack.config.ts
+++ packages/ui/cypress-webpack.config.ts
@@ -5,15 +5,16 @@ const config: webpack.Configuration = {
   devtool: 'source-map',
   mode: 'development', // "production" | "development" | "none"
   entry: './src/index.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.mjs',
-  },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: [ {
+          loader: 'ts-loader',
+          options: {
+            configFile: path.resolve(__dirname, 'tsconfig.json'),
+          },
+        } ],
         exclude: /node_modules/,
       },
       {
