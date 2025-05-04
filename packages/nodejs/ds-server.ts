8e1ef216f76c92446b6ae2c9af11db6f125bfd13
stone <stone-lyl@outlook.com>
2025-05-04 16:41:43 +0800

    feat: add CLI arguments for port and working directory

--- packages/nodejs/ds-server.ts
+++ packages/nodejs/ds-server.ts
@@ -2,10 +2,23 @@ import { nodeJsProvider, SocketServer } from './src';
 import { Application, coreNodeProvider } from '@data-story/core';
 import * as dotenv from 'dotenv';
 import { hubspotProvider } from '@data-story/hubspot';
+import minimist from 'minimist';
 
 dotenv.config({ path: '.env.local' });
+const argv = minimist(process.argv.slice(2), {
+  string: [ 'port', 'workingDir' ],
+  alias: {
+    port: 'p',
+    workingDir: 'w',
+  },
+});
 
-const startServer = async() => {
+const port = argv.port || 3300;
+const workingDir = argv.workingDir || process.cwd();
+
+process.chdir(workingDir);
+
+const startServer = async () => {
   const app = new Application();
 
   app.register([
@@ -18,15 +31,15 @@ const startServer = async() => {
 
   const server = new SocketServer({
     app,
-    port: 3300,
-  })
+    port: port,
+  });
 
-  server.start()
+  server.start();
 };
 
 startServer()
   .then(() => {
-    console.log('Server started');
+    console.log('Working directory', workingDir);
   }).catch((e) => {
     console.error('Error starting server', e);
   });
