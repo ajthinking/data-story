a58301ac308a959ccc822c2777cd602eb7cb2e85
Anders Jürisoo <jurisoo@hotmail.com>
2025-06-06 09:35:34 +0200

    WIP output schemas

--- packages/ui/src/components/DataStory/modals/nodeSettingsModal/tabs/outputSchemas/outputSchemas.tsx
+++ packages/ui/src/components/DataStory/modals/nodeSettingsModal/tabs/outputSchemas/outputSchemas.tsx
@@ -11,6 +11,15 @@ export function OutputSchemas({
     className="flex flex-col"
   >
     <label className="mt-2 mb-1 text-xs text-gray-400">Output Schema</label>
-    <DataStoryOutputTable node={node} register={register} form={form} />
+    {/* <DataStoryOutputTable node={node} register={register} form={form} /> */}
+
+    <input
+      type="text"
+      className="text-xs p-2 w-full bg-gray-50 font-mono"
+      value={JSON.stringify(node.data.outputs)}
+      onChange={(e) => {
+        node.data.outputs = JSON.parse(e.target.value);
+      }}
+    />
   </div>
 }
