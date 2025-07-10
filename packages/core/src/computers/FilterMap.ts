46f14cc76e778e8daef02e519fd5ca0017ff3f14
Anders Jürisoo <jurisoo@hotmail.com>
2025-07-10 10:50:21 +0200

    Add FilterMap node

---
+++ packages/core/src/computers/FilterMap.ts
@@ -0,0 +1,41 @@
+import { jsFn } from '../Param';
+import { multiline } from '../utils/multiline';
+import { Computer } from '../types/Computer';
+import { BatchLimit } from '../utils/batchLimit';
+
+export const FilterMap: Computer = {
+  name: 'FilterMap',
+  label: 'FilterMap',
+  inputs: [{
+    name: 'input',
+    schema: {},
+  }],
+  outputs: [{
+    name: 'output',
+    schema: {},
+  }],
+  params: [
+    jsFn({
+      name: 'mapper',
+      value: multiline`
+        item => ({
+          ...item
+        })
+      `,
+      help: '',
+    }),
+  ],
+
+  async *run({ input, output }) {
+    while(true) {
+      const incoming = input.pull(BatchLimit)
+      const replacers = incoming
+        .map(item => item.params.mapper)
+        .filter(Boolean)
+
+      output.push(replacers)
+
+      yield;
+    }
+  },
+};
